4483759c034841de8107085c4d6f8086e7236058
EvanWashkow <epwashkow@gmail.com>
2018-02-06 22:23:47 -0800

    Add function to update a key in the cache

--- src/PHP/Cache.php
+++ src/PHP/Cache.php
@@ -21,4 +21,54 @@ class Cache
     {
         $this->cache = [];
     }
+    
+    
+    /**
+     * Cache new item, overwriting previous key value
+     *
+     * @param int|string $key   Key to store the value at
+     * @param mixed      $value The value to store
+     * @return int|string Sanitized key. NULL on failure.
+     */
+    public function update( $key, $value )
+    {
+        $key = self::sanatizeKey( $key );
+        if ( isset( $key )) {
+            $this->cache[ $key ] = $value;
+        }
+        return $key;
+    }
+    
+    
+    /**
+     * Sanitize the cache key
+     *
+     * @param mixed $key The cache key
+     * @return mixed NULL will be returned if invalid
+     */
+    final protected static function sanatizeKey( $key )
+    {
+        $type = gettype( $key );
+        
+        // Sanatize strings; attempting to convert strings to integers
+        if ( 'string' == $type ) {
+            $key = trim( $key );
+            if ( 0 !== intval( $key )) {
+                $key = intval( $key );
+            }
+            elseif ( '0' == $key ) {
+                $key = 0;
+            }
+            elseif ( '' == $key ) {
+                $key = NULL;
+            }
+        }
+        
+        // Set key as invalid
+        elseif ( 'integer' != $type ) {
+            $key = NULL;
+        }
+        
+        return $key;
+    }
 }
