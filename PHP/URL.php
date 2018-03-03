41b2f63228ae38e0a41cf5d935506f4db26128b9
EvanWashkow <epwashkow@gmail.com>
2018-03-03 12:54:51 -0800

    Remove "Extract" method from URL

--- PHP/URL.php
+++ PHP/URL.php
@@ -170,7 +170,7 @@ class URL
             }
         }
         return $this->parameters;
-    }
+    }     
     
     
     /**
@@ -181,60 +181,5 @@ class URL
     final public function ToString()
     {
         return $this->url;
-    }
-    
-    
-    /**
-     * Extract the URL into its various pieces
-     *
-     * 
-     * @param string $url         The URL to extract
-     * @param string &$protocol   The resulting protocol ('http'/'https')
-     * @param string &$domain     The resulting domain   ('google.com')
-     * @param string &$path       The resulting path     ('/url/path/structure/')
-     * @param array  &$parameters The resulting GET parameters as a mapped array
-     */
-    final public static function Extract( string $url,
-                                                 &$protocol   = '',
-                                                 &$domain     = '',
-                                                 &$path       = '',
-                                                 &$parameters = [] )
-    {
-        
-        // Exit. Bad URL.
-        $url = self::Sanitize( $url );
-        if ( '' == $url ) {
-            return;
-        }
-        
-        // Set variables
-        $protocol   = '';
-        $domain     = '';
-        $path       = '';
-        $parameters = [];
-        
-        // Extract URL pieces
-        $pieces   = explode( '/', $url );
-        $protocol = array_shift( $pieces );
-        $protocol = substr( $protocol, 0, strlen( $protocol ) - 1 );
-        $domain   = array_shift( $pieces );
-        $domain   = array_shift( $pieces );
-        $_url     = implode( '/', $pieces );
-        $pieces   = explode( '?', $_url );
-        $path     = '/' . $pieces[ 0 ];
-        
-        // Build URL parameters mapped array
-        if ( array_key_exists( 1, $pieces )) {
-            $pieces = explode( '&', $pieces[ 1 ] );
-            foreach ( $pieces as $piece ) {
-                $map   = explode( '=', $piece );
-                $key   = $map[ 0 ];
-                $value = NULL;
-                if ( array_key_exists( 1, $map )) {
-                    $value = $map[ 1 ];
-                }
-                $parameters[ $key ] = $value;
-            }
-        }
     }
 }
