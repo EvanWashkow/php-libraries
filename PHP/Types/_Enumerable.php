9bb29e303e716f9533079bebaba3836ac9b1bf8e
EvanWashkow <epwashkow@gmail.com>
2018-03-07 11:30:11 -0800

    Define _Enumerable ForEach function

--- PHP/Types/_Enumerable.php
+++ PHP/Types/_Enumerable.php
@@ -32,4 +32,13 @@ abstract class _Enumerable extends Object
      * @return int
      */
     abstract public function Count(): int;
+    
+    
+    /**
+     * Interates through each entry, until the end of the list is reached, or a
+     * non-null value is returned from the callback function.
+     *
+     * @param callable $function Callback function( $value, $index )
+     */
+    abstract public function ForEach( callable $function );
 }
