d5484f6edd77bf01197b51c13afdb3a391032684
EvanWashkow <epwashkow@gmail.com>
2018-03-07 10:23:59 -0800

    Add function to determine if the variable is of a certain type

---
+++ PHP/Types/functions.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Determine if the variable is, or is derived from, the type
+ *
+ * @param mixed  $variable The variable
+ * @param string $type     Type string to compare the variable's type with
+ * @return bool
+ */
+function is( $variable, string $type )
+{
+    // Variables
+    $isOfType    = false;
+    $variableType = gettype( $variable );
+    
+    // Evaluate
+    if ( 'object' === $variableType ) {
+        $isOfType = is_a( $variable, $type );
+    }
+    else {
+        $isOfType = $variableType === $type;
+    }
+    return $isOfType;
+}
