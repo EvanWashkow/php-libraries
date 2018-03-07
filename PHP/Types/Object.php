3234ebbee4e4170f27dd51956dc7d933690e938f
EvanWashkow <epwashkow@gmail.com>
2018-03-07 10:31:30 -0800

    Create base Object type definition

---
+++ PHP/Types/Object.php
@@ -0,0 +1,19 @@
+<?php
+namespace PHP\Types;
+
+/**
+ * Base definition for all Object instances
+ */
+class Object
+{
+    
+    /**
+     * Retrieve namespaced string of this object's class type
+     *
+     * @return string
+     */
+    final public function GetType()
+    {
+        return get_class( $this );
+    }
+}
