da248f084d6f496082de7df18f89ff571499ce78
EvanWashkow <epwashkow@gmail.com>
2018-03-07 16:14:40 -0800

    Remove type from _Enumerable constructor

--- PHP/Types/_Enumerable.php
+++ PHP/Types/_Enumerable.php
@@ -8,25 +8,6 @@ abstract class _Enumerable extends Object
 {
     
     /**
-     * Defines type requirement for all entries
-     *
-     * @var string
-     */
-    protected $type;
-    
-    
-    /**
-     * Create a new enumerated instance
-     *
-     * @param string $type Establishes type requirement for all entries. See `is()`.
-     */
-    public function __construct( string $type = '' )
-    {
-        $this->type = $type;
-    }
-    
-    
-    /**
      * Retrieve the number of entries
      *
      * @return int
