a921dc4f3905cb1ad3581938fd7bab3a956f4813
EvanWashkow <epwashkow@gmail.com>
2018-03-08 14:48:43 -0800

    Drop Object->Equals() due to scope creep

--- PHP/Types/Object.php
+++ PHP/Types/Object.php
@@ -8,20 +8,6 @@ class Object
 {
     
     /**
-     * Determines if the given value is equal to this one
-     *
-     * Extend this to return more accurate results for custom objects
-     *
-     * @param mixed $value The value to compare this object to
-     * @return bool
-     */
-    public function Equals( $value ): bool
-    {
-        return ( $this === $value );
-    }
-    
-    
-    /**
      * Retrieve namespaced class string for this type
      *
      * @return string
