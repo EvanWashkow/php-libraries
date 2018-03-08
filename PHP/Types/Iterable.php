962a35612830ae88e13c9fab97ce30740fb8d92b
EvanWashkow <epwashkow@gmail.com>
2018-03-08 08:12:41 -0800

    Define Iterable->Loop()

--- PHP/Types/Iterable.php
+++ PHP/Types/Iterable.php
@@ -35,4 +35,41 @@ class Iterable
     {
         return count( $this->items );
     }
+    
+    
+    /**
+     * For each item in the list, invoke the callback function with its index
+     * and value
+     *
+     * To exit, the loop early, return a non-NULL value. This value will be
+     * returned by Loop().
+     *
+     * Additional arguments can be passed to the callback function by adding
+     * them to Loop(), after the callback function definition. To make edits to
+     * them in the callback function, use the reference identifier `&`.
+     *
+     * @param callable $function Callback function to execute for each item
+     * @param mixed    ...$args  Additional arguments to be passed to the callback function (can be edited by the reference identifier `&` in the callback function)
+     * @return mixed   NULL or the value returned by the callback function
+     */
+    public function Loop( callable $function, &...$args )
+    {
+        foreach ( $this->items as $index => $value ) {
+            
+            // Add index and value the callback function parameters
+            $parameters = array_merge(
+                [
+                    $index,
+                    $value
+                ],
+                $args
+            );
+            
+            // Execute the callback function, exiting when a non-null value is returned
+            $result = call_user_func_array( $function, $parameters );
+            if ( null !== $result ) {
+                return $result;
+            }
+        }
+    }
 }
