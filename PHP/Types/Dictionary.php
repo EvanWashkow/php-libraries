381d0ba4aa928d5cc80afba5b98dd0040c263bf4
EvanWashkow <epwashkow@gmail.com>
2018-03-09 09:04:21 -0800

    Stub a Dictionary type

---
+++ PHP/Types/Dictionary.php
@@ -0,0 +1,16 @@
+<?php
+namespace PHP\Types;
+
+/**
+ * Defines a mutable, unordered set of indexed values
+ */
+class Dictionary extends Object
+{
+    
+    /**
+     * The set of indexed values
+     *
+     * @var array
+     */
+    private $items;
+}
