2c49d16aafcdc543a83377adef35a244dc41868a
EvanWashkow <epwashkow@gmail.com>
2018-03-09 11:48:23 -0800

    Define Dictionary->Loop()

--- PHP/Types/Dictionary.php
+++ PHP/Types/Dictionary.php
@@ -138,6 +138,43 @@ class Dictionary extends Object
     
     
     /**
+     * Iterate through every item, invoking the callback function with the
+     * item's index and value
+     *
+     * To exit the loop early, return a non-NULL value. This value will also be
+     * returned by Loop().
+     *
+     * Additional arguments can be passed to the callback function by adding
+     * them to Loop(), after the callback function definition. To make edits to
+     * them in the callback function, use the reference identifier `&`.
+     *
+     * @param callable $function Callback function to execute for each item
+     * @param mixed    ...$args  Additional arguments to be passed to the callback function (can be edited by the reference identifier `&` in the callback function)
+     * @return mixed   NULL or the value returned by the callback function
+     */
+    public function Loop( callable $function, &...$args )
+    {
+        foreach ( $this->items as $index => $value ) {
+            
+            // Add index and value the callback function parameters
+            $parameters = array_merge(
+                [
+                    $index,
+                    $value
+                ],
+                $args
+            );
+            
+            // Execute the callback function, exiting when a non-null value is returned
+            $result = call_user_func_array( $function, $parameters );
+            if ( null !== $result ) {
+                return $result;
+            }
+        }
+    }
+    
+    
+    /**
      * Store the value at the index, overwriting any pre-existing values
      *
      * Fails if the index or value doesn't match its type requirement
