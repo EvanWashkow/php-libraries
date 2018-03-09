f6cd320abccec254dcc1aeb57604ab80a68ae38d
EvanWashkow <epwashkow@gmail.com>
2018-03-09 12:47:10 -0800

    Define basic structure for all indexed values

---
+++ PHP/Types/_IndexedValues.php
@@ -0,0 +1,9 @@
+<?php
+namespace PHP\Types;
+
+/**
+ * Defines structure for a set of indexed, mutable values
+ */
+abstract class _IndexedValues extends _IndexedValues\_ReadOnly
+{
+}
---
+++ PHP/Types/_IndexedValues/_ReadOnly.php
@@ -0,0 +1,9 @@
+<?php
+namespace PHP\Types\_IndexedValues;
+
+/**
+ * Defines structure for a set of indexed, read-only values
+ */
+abstract class _ReadOnly extends \PHP\Types\Object
+{
+}
