4ab4863ff2c7e5f741838668d9f0c942523864f6
EvanWashkow <epwashkow@gmail.com>
2018-03-09 09:16:40 -0800

    Define Dictionary->__construct() having index and value types

--- PHP/Types/Dictionary.php
+++ PHP/Types/Dictionary.php
@@ -13,4 +13,32 @@ class Dictionary extends Object
      * @var array
      */
     private $items;
+    
+    /**
+     * Specifies the type requirement for all indexes
+     *
+     * @var string
+     */
+    private $indexType;
+    
+    /**
+     * Specifies the type requirement for all values
+     *
+     * @var string
+     */
+    private $valueType;
+    
+    
+    /**
+     * Create a new Dictionary instance
+     *
+     * @param string $indexType Specifies the type requirement for all indexes (see `is()`). An empty string permits all types.
+     * @param string $valueType Specifies the type requirement for all values (see `is()`). An empty string permits all types.
+     */
+    public function __construct( string $indexType = '', string $valueType = '' )
+    {
+        $this->items     = [];
+        $this->indexType = $indexType;
+        $this->valueType = $valueType;
+    }
 }
