a76e8efbb5652b078e6bdbe3784992ed8c1599ad
EvanWashkow <epwashkow@gmail.com>
2018-03-13 10:02:39 -0700

    Define interface for an iterable type

--- PHP/Collections/Iterable.php
+++ PHP/Collections/Iterable.php
@@ -1,10 +1,12 @@
 <?php
 namespace PHP\Collections;
 
+use PHP\Collections\Iterable\iIterable;
+
 /**
  * Defines a set of items that can be iterated over
  */
-class Iterable extends \PHP\Object
+class Iterable extends \PHP\Object implements iIterable
 {
     
     /**
@@ -26,21 +28,6 @@ class Iterable extends \PHP\Object
     }
     
     
-    /**
-     * Iterate through every item, invoking the callback function with the item's
-     * index and value
-     *
-     * To exit, the loop early, return a non-NULL value. This value will be
-     * returned by Loop().
-     *
-     * Additional arguments can be passed to the callback function by adding
-     * them to Loop(), after the callback function definition. To make edits to
-     * them in the callback function, use the reference identifier `&`.
-     *
-     * @param callable $function Callback function to execute for each item
-     * @param mixed    ...$args  Additional arguments to be passed to the callback function (can be edited by the reference identifier `&` in the callback function)
-     * @return mixed   NULL or the value returned by the callback function
-     */
     public function Loop( callable $function, &...$args )
     {
         foreach ( $this->items as $index => $value ) {
---
+++ PHP/Collections/Iterable/iIterable.php
@@ -0,0 +1,28 @@
+<?php
+namespace PHP\Collections\Iterable;
+
+use PHP\Object\iObject;
+
+/**
+ * Defines the type for a set of items that can be iterated over
+ */
+interface iIterable extends iObject
+{
+    
+    /**
+     * Iterate through every item, invoking the callback function with the item's
+     * index and value
+     *
+     * To exit, the loop early, return a non-NULL value. This value will be
+     * returned by Loop().
+     *
+     * Additional arguments can be passed to the callback function by adding
+     * them to Loop(), after the callback function definition. To make edits to
+     * them in the callback function, use the reference identifier `&`.
+     *
+     * @param callable $function Callback function to execute for each item
+     * @param mixed    ...$args  Additional arguments to be passed to the callback function (can be edited by the reference identifier `&` in the callback function)
+     * @return mixed   NULL or the value returned by the callback function
+     */
+    public function Loop( callable $function, &...$args );
+}
