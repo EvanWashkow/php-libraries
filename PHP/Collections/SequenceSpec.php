728b2b8f03d80c435854f5ab5376bfc1da999914
EvanWashkow <epwashkow@gmail.com>
2018-03-14 15:30:34 -0700

    Update Sequence method documentation to prompt IDE auto-completion

--- PHP/Collections/SequenceSpec.php
+++ PHP/Collections/SequenceSpec.php
@@ -2,6 +2,7 @@
 namespace PHP\Collections;
 
 use PHP\Collections\Sequence\ReadOnlySequenceSpec;
+use PHP\Collections\Collection\ReadOnlyCollectionSpec;
 
 /**
  * Specifications for a mutable, ordered set of indexed values
@@ -18,6 +19,13 @@ interface SequenceSpec extends CollectionSpec, ReadOnlySequenceSpec
     public function Add( $value ): int;
     
     /**
+     * Duplicate every index and value into a new instance
+     *
+     * @return SequenceSpec
+     */
+    public function Clone(): ReadOnlyCollectionSpec;
+    
+    /**
      * Insert the value at the index, shifting remaining values up
      *
      * @param int   $index The index to insert the value at
@@ -30,6 +38,24 @@ interface SequenceSpec extends CollectionSpec, ReadOnlySequenceSpec
      * Put all entries in reverse order
      */
     public function Reverse();
+    
+    /**
+     * Create a subset of entries from this one
+     *
+     * @param int $start Starting index
+     * @param int $end   Ending index
+     * @return SequenceSpec
+     */
+    public function Slice( int $start, int $end ): ReadOnlySequenceSpec;
+    
+    /**
+     * Chop these entries into groups, using the given value as a delimiter
+     *
+     * @param mixed $delimiter Value separating each group
+     * @param int   $limit     Maximum number of entries to return; negative to return all.
+     * @return SequenceSpec
+     */
+    public function Split( $delimiter, int $limit = -1 ): ReadOnlySequenceSpec;
     
     /**
      * Overwrite the value at the index, if it exists
