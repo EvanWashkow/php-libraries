51554a87859854eace0589732ee68da4192a4eda
EvanWashkow <epwashkow@gmail.com>
2018-04-06 15:28:49 -0700

    Replace "index" and "indices" in Cache

--- PHP/Cache.php
+++ PHP/Cache.php
@@ -20,12 +20,12 @@ class Cache extends Dictionary implements CacheSpec
     /**
      * Create a new cache instance
      *
-     * @param string $indexType Specifies the type requirement for all indices (see `is()`). An empty string permits all types. Must be 'string' or 'integer'.
+     * @param string $keyType Specifies the type requirement for all keys (see `is()`). An empty string permits all types. Must be 'string' or 'integer'.
      * @param string $valueType Specifies the type requirement for all values (see `is()`). An empty string permits all types.
      */
-    public function __construct( string $indexType = '', string $valueType = '' )
+    public function __construct( string $keyType = '', string $valueType = '' )
     {
-        parent::__construct( $indexType, $valueType );
+        parent::__construct( $keyType, $valueType );
         $this->markIncomplete();
     }
     
