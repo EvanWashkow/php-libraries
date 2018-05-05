8b7ed496327dde99540ae23009fcb284b3ea4a43
EvanWashkow <epwashkow@gmail.com>
2018-05-05 14:18:21 -0700

    Test for StackTrace::Get() returning a non-empty array

--- tests/PHP/Debug/StackTraceTest.php
+++ tests/PHP/Debug/StackTraceTest.php
@@ -1,14 +1,32 @@
 <?php
 
+/**
+ * StackTrace tests
+ */
 class StackTraceTest extends \PHPUnit\Framework\TestCase
 {
     
+    /**
+     * Does Get() return an array?
+     */
     public function testGetReturnsAnArray()
     {
         $this->assertInternalType(
             'array',
             \PHP\Debug\StackTrace::Get(),
-            'StackTrace::Get() didn\'t return an array'
+            'StackTrace::Get() didn\'t return an array, as expected'
+        );
+    }
+    
+    
+    /**
+     * Ensure Get() array is not empty
+     */
+    public function testGetReturnsNonEmptyArray()
+    {
+        $this->assertTrue(
+            ( 0 !== count( \PHP\Debug\StackTrace::Get() )),
+            'StackTrace::Get() returned an empty array. There should be some entries.'
         );
     }
 }
