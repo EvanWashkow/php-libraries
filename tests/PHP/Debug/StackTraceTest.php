d8151e22986e4fcc89d0b321dd0223eac6b77a10
EvanWashkow <epwashkow@gmail.com>
2018-05-05 14:43:01 -0700

    Test StackTrace::ToString() is non-empty

--- tests/PHP/Debug/StackTraceTest.php
+++ tests/PHP/Debug/StackTraceTest.php
@@ -18,7 +18,7 @@ class StackTraceTest extends \PHPUnit\Framework\TestCase
         $this->assertInternalType(
             'array',
             \PHP\Debug\StackTrace::Get(),
-            'StackTrace::Get() didn\'t return an array, as expected'
+            "StackTrace::Get() didn't return an array, as expected"
         );
     }
     
@@ -30,7 +30,7 @@ class StackTraceTest extends \PHPUnit\Framework\TestCase
     {
         $this->assertTrue(
             ( 0 !== count( \PHP\Debug\StackTrace::Get() )),
-            'StackTrace::Get() returned an empty array. There should be some entries.'
+            "StackTrace::Get() returned an empty array"
         );
     }
     
@@ -49,7 +49,19 @@ class StackTraceTest extends \PHPUnit\Framework\TestCase
         $this->assertInternalType(
             'string',
             \PHP\Debug\StackTrace::ToString(),
-            'StackTrace::ToString() didn\'t return a string, as expected'
+            "StackTrace::ToString() didn\'t return a string, as expected"
+        );
+    }
+    
+    
+    /**
+     * Ensure ToString() result is not empty
+     */
+    public function testToStringReturnsNonEmptyString()
+    {
+        $this->assertTrue(
+            ( 0 !== strlen( \PHP\Debug\StackTrace::ToString() )),
+            "StackTrace::ToString() returned an empty string"
         );
     }
 }
