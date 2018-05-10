7d28f78b542fcef84eba684ce5372e004ce4a2c3
EvanWashkow <epwashkow@gmail.com>
2018-05-09 22:53:44 -0700

    Define read only dictionary test data

---
+++ tests/PHP/Collections/ReadOnlyDictionaryData.php
@@ -0,0 +1,53 @@
+<?php
+
+require_once( __DIR__ . '/DictionaryData.php' );
+
+use PHP\Collections\ReadOnlyDictionary;
+
+/**
+ * Dictionary Data for testing
+ */
+class ReadOnlysDictionaryData
+{
+    
+    /**
+     * Retrieve all test dictionaries
+     *
+     * @return array
+     */
+    public static function Get(): array
+    {
+        return array_merge(
+            self::GetTyped(),
+            [
+                self::GetMixed()
+            ]
+        );
+    }
+    
+    /**
+     * Retrieve all test typed dictionaries
+     *
+     * @return array
+     */
+    public static function GetTyped(): array
+    {
+        $dictionaries = [];
+        foreach ( DictionaryData::GetTyped() as $dictionary ) {
+            $dictionaries[] = new ReadOnlyDictionary( $dictionary );
+        }
+        return $dictionaries;
+    }
+    
+    
+    /**
+     * Retrieve sample Dictionary with mixed string and value types
+     *
+     * @return ReadOnlyDictionary
+     */
+    public static function GetMixed(): ReadOnlyDictionary
+    {
+        $dictionary = DictionaryData::GetMixed();
+        return new ReadOnlyDictionary( $dictionary );
+    }
+}
