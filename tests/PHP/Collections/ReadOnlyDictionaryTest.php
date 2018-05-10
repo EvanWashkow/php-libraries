cbd4758cafffebcaa6aa5646ec0d2ada13aa90b9
EvanWashkow <epwashkow@gmail.com>
2018-05-09 23:12:42 -0700

    Test Dictionary cloning

---
+++ tests/PHP/Collections/ReadOnlyDictionaryTest.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once( __DIR__ . '/ReadOnlyDictionaryData.php' );
+
+/**
+ * Dictionary Tests
+ *
+ * This only tests the editable portion of a Dictionary. See ReadOnlyDictionary
+ * for the read accessors
+ */
+class ReadOnlyDictionaryTest extends \PHPUnit\Framework\TestCase
+{
+    
+    /**
+     * Cloning a dictionary should clone all the entries
+     */
+    public function testCloneCopiesAllEntries()
+    {
+        foreach ( ReadOnlysDictionaryData::Get() as $dictionary ) {
+            $clone = $dictionary->clone();
+            $clone->loop( function( $key, $value ) use ( $dictionary ) {
+                $this->assertTrue(
+                    $dictionary->hasKey( $key ),
+                    "Dictionary->clone() entry does not have the same key"
+                );
+                $this->assertEquals(
+                    $value,
+                    $dictionary->get( $key ),
+                    "Dictionary->clone() entry does not have the same value"
+                );
+            });
+        }
+    }
+}
