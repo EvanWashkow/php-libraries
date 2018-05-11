a04869f79375989d60969c48356a6bd4aa5a68f7
EvanWashkow <epwashkow@gmail.com>
2018-05-10 22:50:29 -0700

    Test ReadOnlyDictionary->get()

--- tests/PHP/Collections/ReadOnlyDictionaryTest.php
+++ tests/PHP/Collections/ReadOnlyDictionaryTest.php
@@ -5,14 +5,11 @@ require_once( __DIR__ . '/ReadOnlyDictionaryData.php' );
 /**
  * Dictionary Tests
  *
- * This only tests the editable portion of a Dictionary. See ReadOnlyDictionary
- * for the read accessors
+ * This tests both the read-only and the editable dictionary (the read-only
+ * simply invokes the editable)
  */
 class ReadOnlyDictionaryTest extends \PHPUnit\Framework\TestCase
 {
-    
-    
-    
     
     /***************************************************************************
     *                         ReadOnlyDictionary->clone()
@@ -90,5 +87,57 @@ class ReadOnlyDictionaryTest extends \PHPUnit\Framework\TestCase
             $dictionary->count(),
             "ReadOnlyDictionary->count() returned {$dictionary->count()} on an empty Dictionary"
         );
+    }
+    
+    
+    
+    
+    /***************************************************************************
+    *                         ReadOnlyDictionary->get()
+    ***************************************************************************/
+    
+    /**
+     * A value should be retrievable by its key
+     */
+    public function testGetRetrievesValue()
+    {
+        foreach ( ReadOnlyDictionaryData::Get() as $dictionary ) {
+            $key;
+            $value;
+            $dictionary->loop(function($k, $v) use ( &$key, &$value ) {
+                $key   = $k;
+                $value = $v;
+                return -1;
+            });
+            $this->assertEquals(
+                $value,
+                $dictionary->get( $key ),
+                "ReadOnlyDictionary->get() did not return the value corresponding to its key"
+            );
+        }
+    }
+    
+    
+    /**
+     * Should not be able to retrieve a value from the dictionary with the wrong
+     * key type
+     */
+    public function testTypedDictionaryGetWithWrongKeyType()
+    {
+        foreach ( ReadOnlyDictionaryData::GetTyped() as $dictionary ) {
+            $isSuccessful = false;
+            $dictionary->loop(function( $key, $value ) use ( &$isSuccessful ) {
+                try {
+                    $isSuccessful = $dictionary->get( $value );
+                } catch (\Exception $e) {}
+                if ( $isSuccessful ) {
+                    return $isSuccessful;
+                }
+            });
+            $this->assertFalse(
+                $isSuccessful,
+                "ReadOnlyDictionary->get() should not be able to retrieve with the wrong type"
+            );
+        }
     }
 }
