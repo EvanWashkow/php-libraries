88c1afecea8dba276395aa1b16cf4429848a18f7
EvanWashkow <epwashkow@gmail.com>
2018-06-25 15:40:26 -0700

    Add typed and mixed read-only collection tests

--- tests/PHP/Collections/ReadOnlyCollectionData.php
+++ tests/PHP/Collections/ReadOnlyCollectionData.php
@@ -9,7 +9,7 @@ final class ReadOnlyCollectionData
 {
     
     /**
-     * Get all test data for a read-only collection
+     * Get all test data for read-only collections
      * 
      * @return array
      */
@@ -18,5 +18,33 @@ final class ReadOnlyCollectionData
         return array_merge(
             ReadOnlyDictionaryData::Get()
         );
+    }
+    
+    
+    /**
+     * Get all typed test data for read-only collections
+     * 
+     * @return array
+     */
+    public function GetTyped()
+    {
+        return array_merge(
+            ReadOnlyDictionaryData::GetTyped()
+        );
+    }
+    
+    
+    /**
+     * Get all mixed test data for read-only collections
+     *
+     * NOTE: Do not return sequences, since their keys are not mixed
+     * 
+     * @return array
+     */
+    public function GetMixed()
+    {
+        return [
+            ReadOnlyDictionaryData::GetMixed()
+        ];
     }
 }
