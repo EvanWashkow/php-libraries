8ca99c73508d03e14b26fe881ac13662d94abbcc
EvanWashkow <epwashkow@gmail.com>
2018-06-25 14:02:24 -0700

    Test if ReadOnlyCollection->getKeys() matches the actual keys

--- tests/PHP/Collections/ReadOnlyCollectionTest.php
+++ tests/PHP/Collections/ReadOnlyCollectionTest.php
@@ -17,11 +17,30 @@ class ReadOnlyCollectionTest extends \PHPUnit\Framework\TestCase
      */
     public function testGetKeysReturnsSequence()
     {
-        foreach ( ReadOnlyCollectionData::Get() as $dictionary ) {
+        foreach ( ReadOnlyCollectionData::Get() as $collection ) {
             $this->assertInstanceOf(
                 "PHP\\Collections\\Sequence",
-                $dictionary->getKeys(),
-                "Expected Sequence to be returned from ReadOnlyDictionary->getKeys()"
+                $collection->getKeys(),
+                "Expected Sequence to be returned from ReadOnlyCollection->getKeys()"
+            );
+        }
+    }
+    
+    
+    /**
+     * Does getKeys() return the collection's keys?
+     */
+    public function testGetKeysReturnsKeys()
+    {
+        foreach ( ReadOnlyCollectionData::Get() as $collection ) {
+            $keys = [];
+            $collection->loop(function( $key, $value ) use ( &$keys ) {
+                $keys[] = $key;
+            });
+            $this->assertEquals(
+                $keys,
+                $collection->getKeys()->toArray(),
+                "ReadOnlyCollection->getKeys() doesn't match the keys inside the collection."
             );
         }
     }
