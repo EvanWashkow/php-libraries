4754ef15023c92071380af49b96fc219f3fd9d00
EvanWashkow <epwashkow@gmail.com>
2018-07-06 14:29:26 -0700

    Move remove() tests to the collection's test class

--- tests/PHP/Collections/CollectionTest.php
+++ tests/PHP/Collections/CollectionTest.php
@@ -13,7 +13,7 @@ class CollectionTest extends \PHPUnit\Framework\TestCase
     ***************************************************************************/
     
     /**
-     * Test if clearing the dictionary has a count of zero
+     * Test if clearing the collection has a count of zero
      */
     public function testClearHaveNoEntries()
     {
@@ -24,6 +24,95 @@ class CollectionTest extends \PHPUnit\Framework\TestCase
                 0,
                 $collection->count(),
                 "Expected {$name}->clear() to remove all elements"
+            );
+        }
+    }
+    
+    
+    
+    
+    /***************************************************************************
+    *                           Collection->remove()
+    ***************************************************************************/
+    
+    /**
+     * Does removing a key from the collection remove the key?
+     */
+    public function testRemoveHasSmallerCount()
+    {
+        foreach ( CollectionData::GetNonEmpty() as $collection ) {
+            $previous = $collection->count();
+            foreach ( $collection as $key => $value ) {
+                $collection->remove( $key );
+                break;
+            }
+            $after = $collection->count();
+            $this->assertLessThan(
+                $previous,
+                $after,
+                "Dictionary->remove( 'a' ) has the same number of keys as before"
+            );
+        }
+    }
+    
+    
+    /**
+     * Does removing a key with a non-existing key fail?
+     */
+    public function testRemoveThrowsExceptionWithMissingKey()
+    {
+        foreach ( CollectionData::GetNonEmpty() as $collection ) {
+            $previous = $collection->count();
+            $isError  = false;
+            try {
+                $collection->remove( 'foobar' );
+            } catch (\Exception $e) {
+                $isError = true;
+            }
+            $after = $collection->count();
+            
+            $this->assertEquals(
+                $previous,
+                $after,
+                "Dictionary->remove() should not be able to remove a key that doesn't exist"
+            );
+            $this->assertTrue(
+                $isError,
+                "Dictionary->remove() did not produce an error when invoked with a non-existing key"
+            );
+        }
+    }
+    
+    
+    /**
+     * Does removing a key with the wrong key type fail?
+     */
+    public function testRemoveThrowsExceptionWithWrongKeyType()
+    {
+        foreach ( CollectionData::GetTyped() as $collection ) {
+            $key;
+            foreach ( $collection as $k => $value ) {
+                $key = $value;
+                break;
+            }
+            
+            $previous = $collection->count();
+            $isError  = false;
+            try {
+                $collection->remove( $key );
+            } catch ( \Exception $e ) {
+                $isError = true;
+            }
+            $after = $collection->count();
+            
+            $this->assertEquals(
+                $previous,
+                $after,
+                "Dictionary->remove() should not be able to remove a key with the wrong type"
+            );
+            $this->assertTrue(
+                $isError,
+                "Dictionary->remove() did not produce an error when invoked with the wrong key type"
             );
         }
     }
