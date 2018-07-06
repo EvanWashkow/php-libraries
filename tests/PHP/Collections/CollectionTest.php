2270e546c0cf9517d670061916a1ac2e79ead74b
EvanWashkow <epwashkow@gmail.com>
2018-07-06 11:40:01 -0700

    Create collection test class

---
+++ tests/PHP/Collections/CollectionTest.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once( __DIR__ . '/CollectionData.php' );
+
+/**
+ * Test all Collection methods to ensure consistent functionality
+ */
+class CollectionTest extends \PHPUnit\Framework\TestCase
+{
+    
+}
