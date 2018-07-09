1cdc46eeeb590d88d78f4230578043858c63f479
EvanWashkow <epwashkow@gmail.com>
2018-07-09 11:42:14 -0700

    Stub read-only sequence test class

---
+++ tests/PHP/Collections/ReadOnlySequenceTest.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once( __DIR__ . '/ReadOnlySequenceData.php' );
+
+/**
+ * Test all ReadOnlySequence methods to ensure consistent functionality
+ *
+ * This tests both the read-only and the editable sequnce (the read-only
+ * simply invokes the editable)
+ */
+class ReadOnlySequenceTest extends \PHPUnit\Framework\TestCase
+{
+    
+}
