dc992886f574de08db98e065231c422f3d8e86be
EvanWashkow <epwashkow@gmail.com>
2018-07-09 10:49:05 -0700

    Add Sequence test data

---
+++ tests/PHP/Collections/SequenceData.php
@@ -0,0 +1,129 @@
+<?php
+
+use PHP\Collections\Sequence;
+
+/**
+ * Sequence test data
+ */
+class SequenceData
+{
+    
+    /***************************************************************************
+    *                                 MAIN
+    ***************************************************************************/
+    
+    /**
+     * Retrieve sample sequence data that has no entries
+     *
+     * @return array
+     */
+    public static function GetEmpty(): array
+    {
+        $sequences = [];
+        foreach ( self::GetNonEmpty() as $sequence ) {
+            $sequence->clear();
+            $sequences[] = $sequence;
+        }
+        return $sequences;
+    }
+    
+    
+    /**
+    * Retrieve all non-empty test sequences
+    *
+    * @return array
+    */
+    public static function GetNonEmpty(): array
+    {
+        return array_merge(
+            self::GetTyped(),
+            self::GetMixed()
+        );
+    }
+    
+    
+    /**
+    * Retrieve sample Sequence with mixed string and value types
+    *
+    * @return array
+    */
+    public static function GetMixed(): array
+    {
+        $sequence = new Sequence();
+        foreach ( self::GetTyped() as $typedSequence ) {
+            $typedSequence->loop( function( $key, $value ) use ( &$sequence ) {
+                $sequence->add( $value );
+            });
+        }
+        return [
+            $sequence
+        ];
+    }
+    
+    
+    /**
+     * Retrieve all test typed sequences
+     *
+     * IMPORTANT: Key and value types must be different. It is useful to swap
+     * key / values as parameters to test type constraints. It is best to define
+     * similar entries that PHP usually chokes on, such as "1" => 1
+     *
+     * @return array
+     */
+    public static function GetTyped(): array
+    {
+        return [
+            self::getBool(),
+            self::getString(),
+            self::getObject()
+        ];
+    }
+    
+    
+    /***************************************************************************
+    *                                   TYPED
+    ***************************************************************************/
+    
+    /**
+     * Return sample Sequence with 0, 1 => false, true
+     *
+     * @return Sequence
+     */
+    private static function getBool(): Sequence
+    {
+        $sequence = new Sequence( 'boolean' );
+        $sequence->add( false );
+        $sequence->add( true );
+        return $sequence;
+    }
+    
+    
+    /**
+     * Return sample Sequence with 0-2 => "0"-"2"
+     *
+     * @return Sequence
+     */
+    private static function getString(): Sequence
+    {
+        $sequence = new Sequence( 'string' );
+        for ( $i = 0; $i <= 2; $i++ ) {
+            $sequence->add( (string) $i );
+        }
+        return $sequence;
+    }
+    
+    
+    /**
+     * Return sample Sequence with 0-2 => new stdClass()
+     *
+     * @return Sequence
+     */
+    private static function getObject(): Sequence
+    {
+        $sequence = new Sequence( 'stdClass' );
+        for ( $i = 0; $i <= 2; $i++ ) {
+            $sequence->add( new stdClass() );
+        }
+        return $sequence;
+    }
+}
