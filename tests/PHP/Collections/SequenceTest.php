ccd1bd8b4db1a68bdd459f56ab698bec5d2e651e
EvanWashkow <epwashkow@gmail.com>
2018-07-17 16:49:00 -0700

    Ensure Sequence->add() has the same value at the end

--- tests/PHP/Collections/SequenceTest.php
+++ tests/PHP/Collections/SequenceTest.php
@@ -33,4 +33,50 @@ class SequenceTest extends \PHP\Tests\Collections\CollectionsTestCase
             );
         }
     }
+    
+    
+    /**
+     * Ensure Sequence->add() has a higher count
+     */
+    public function testAddErrorsOnWrongType()
+    {
+        foreach ( SequenceData::GetTyped() as $sequence ) {
+            if ( 0 === $sequence->count() ) {
+                continue;
+            }
+            $isError = false;
+            try {
+                $sequence->add( $sequence->getFirstKey() );
+            }
+            catch ( \Exception $e ) {
+                $isError = true;
+            }
+            $class = self::getClassName( $sequence );
+            $this->assertTrue(
+                $isError,
+                "Expected {$class}->add() to error on the wrong type"
+            );
+        }
+    }
+    
+    
+    /**
+     * Ensure Sequence->add() has the same value at the end
+     */
+    public function testAddValueIsSame()
+    {
+        foreach ( SequenceData::Get() as $sequence ) {
+            if ( 0 === $sequence->count() ) {
+                continue;
+            }
+            $value = $sequence->get( $sequence->getLastKey() );
+            $sequence->add( $value );
+            $class = self::getClassName( $sequence );
+            $this->assertTrue(
+                $value === $sequence->get( $sequence->getLastKey() ),
+                $sequence->count(),
+                "Expected {$class}->add() to have the same value at the end"
+            );
+        }
+    }
 }
