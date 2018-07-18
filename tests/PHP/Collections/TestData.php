fe3df716a795cdba8647f0df6788c966c8669e36
EvanWashkow <epwashkow@gmail.com>
2018-07-18 10:55:58 -0700

    Create generic collection test data

---
+++ tests/PHP/Collections/TestData.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace PHP\Tests\Collections;
+
+/**
+ * Test data mapped by type
+ *
+ * Use this going forward for all future collection tests.
+ */
+final class TestData
+{
+    
+    /**
+     * Retrieve a mapped array of sequence type => test data
+     * 
+     * @return array
+     */
+    public static function Get(): array
+    {
+        $typeMap = [
+            'integer' => [
+                0,
+                1
+            ],
+            'string' => [
+                '0',
+                '1'
+            ],
+            'boolean' => [
+                false,
+                true
+            ],
+            'stdClass' => [
+                new \stdClass(),
+                (object) [ 0, 1 ]
+            ],
+            '' => [
+                null
+            ]
+        ];
+        foreach ( $typeMap as $type => $values ) {
+            if ( '' !== $type ) {
+                $typeMap[ '' ] = array_merge(
+                    $typeMap[ '' ],
+                    $values
+                );
+            }
+        }
+        return $typeMap;
+    }
+}
