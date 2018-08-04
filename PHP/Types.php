eb0f248fbb2067418cd8577bd15633174df60134
EvanWashkow <epwashkow@gmail.com>
2018-08-04 15:12:29 -0700

    Add basic type lookup by value

--- PHP/Types.php
+++ PHP/Types.php
@@ -7,4 +7,38 @@ namespace PHP;
 final class Types
 {
     
+    /**
+     * Retrieve the type information for the given value
+     *
+     * @param mixed $value The value to retrieve type information for
+     * @return Types\Type
+     */
+    public static function GetByValue( $value ): Types\Type
+    {
+        // Get and convert the name
+        $name = gettype( $value );
+        switch ( $name ) {
+            case 'boolean':
+                $name = 'bool';
+                break;
+            
+            case 'integer':
+                $name = 'int';
+                break;
+            
+            case 'double':
+                $name = 'float';
+                break;
+            
+            case 'object':
+                $name = get_class( $value );
+                break;
+            
+            default:
+                break;
+        }
+        
+        // Create and return a new type
+        return new Types\Type( $name );
+    }
 }
