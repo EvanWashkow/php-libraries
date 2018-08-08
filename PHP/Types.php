b413400a4df766d37763587b7f1ad0811aec1eb4
EvanWashkow <epwashkow@gmail.com>
2018-08-08 09:52:17 -0700

    Implement Types::GetByName()

--- PHP/Types.php
+++ PHP/Types.php
@@ -7,41 +7,49 @@ namespace PHP;
 final class Types
 {
     
+    
     /**
-     * Retrieve the type information for the given value
+     * Retrieve the type information by name
+     *
+     * @param string $name The type name
+     * @return Types\Type
+     */
+    public static function GetByName( string $name ): Types\Type
+    {
+        $name      = trim( $name );
+        $shortName = '';
+        if ( 'boolean' === $name ) {
+            $shortName = 'bool';
+        }
+        elseif ( 'double' === $name ) {
+            $shortName = 'float';
+        }
+        elseif ( 'integer' === $name ) {
+            $shortName = 'int';
+        }
+        elseif ( 'NULL' === $name ) {
+            $name = 'null';
+        }
+        elseif ( class_exists( $name )) {
+            $shortName = explode( '\\', $name );
+            $shortName = array_pop( $shortName );
+        }
+        return new Types\Type( $name, $shortName );
+    }
+    
+    
+    /**
+     * Retrieve the type information by value
      *
      * @param mixed $value The value to retrieve type information for
      * @return Types\Type
      */
     public static function GetByValue( $value ): Types\Type
     {
-        // Get and convert the name
-        $name      = strtolower( gettype( $value ) );
-        $shortName = '';
-        switch ( $name ) {
-            case 'boolean':
-                $shortName = 'bool';
-                break;
-            
-            case 'integer':
-                $shortName = 'int';
-                break;
-            
-            case 'double':
-                $shortName = 'float';
-                break;
-            
-            case 'object':
-                $name      = get_class( $value );
-                $shortName = explode( '\\', $name );
-                $shortName = array_pop( $shortName );
-                break;
-            
-            default:
-                break;
+        $name = strtolower( gettype( $value ) );
+        if ( 'object' === $name ) {
+            $name = get_class( $value );
         }
-        
-        // Create and return a new type
-        return new Types\Type( $name, $shortName );
+        return self::GetByName( $name );
     }
 }
