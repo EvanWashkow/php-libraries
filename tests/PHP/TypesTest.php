c74e6feeac375f3b735e978af73e05d2bc98184b
EvanWashkow <epwashkow@gmail.com>
2018-08-15 13:43:53 -0700

    Ensure Types::GetByValue() returns a Type instance

--- tests/PHP/TypesTest.php
+++ tests/PHP/TypesTest.php
@@ -1,10 +1,29 @@
 <?php
 namespace PHP\Tests;
 
+use PHP\Types;
+
+require_once( __DIR__ . '/TypesData.php' );
+
 /**
  * Tests the \PHP\Types functionality
  */
 class TypesTest extends TestCase
 {
     
+    /**
+     * Ensure ::GetByValue() returns a `Type` instance
+     */
+    public function testGetByValueReturnsType()
+    {
+        foreach ( TypesData::Get() as $data ) {
+            if ( array_key_exists( 'value', $data['in'] )) {
+                $this->assertInstanceOf(
+                    'PHP\\Types\\Type',
+                    Types::GetByValue( $data['in']['value'] ),
+                    'Expected Types::GetByValue() to return a PHP\\Types\\Type instance'
+                );
+            }
+        }
+    }
 }
