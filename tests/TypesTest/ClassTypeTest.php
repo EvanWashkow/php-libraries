8a7bc3f1623db5feacd31daa5f046f67cc6c0033
EvanWashkow <epwashkow@gmail.com>
2018-10-03 21:44:13 -0700

    Stub the class type test

---
+++ tests/TypesTest/ClassTypeTest.php
@@ -0,0 +1,19 @@
+<?php
+namespace PHP\Tests\TypesTest;
+
+use PHP\Types;
+
+/**
+ * Tests the \PHP\Types\ClassType functionality
+ */
+class ClassTypeTest extends \PHP\Tests\TestCase
+{
+
+
+    /***************************************************************************
+    *                            ClassType->getName()
+    *
+    * This was already tested when testing type lookup in TypesTest. Nothing to
+    * do here.
+    ***************************************************************************/
+}
