04ce3141ee2e8c095e5d5c427994201776064591
EvanWashkow <epwashkow@gmail.com>
2018-11-15 10:53:22 -0800

    Add static class to define all type names

---
+++ PHP/Types/TypeNames.php
@@ -0,0 +1,11 @@
+<?php
+namespace PHP\Types;
+
+/**
+ * Defines list of known type names
+ */
+final class TypeNames
+{
+    /** @var string CALLABLE_TYPE_NAME Base name for any "callable" type (http://php.net/manual/en/language.types.callable.php) */
+    const CALLABLE_TYPE_NAME = 'callable';
+}
