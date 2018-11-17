362bfd927bc1e16089108eea646ff811dc7976b7
EvanWashkow <epwashkow@gmail.com>
2018-11-16 16:49:11 -0800

    Stub callable class type

---
+++ PHP/Types/Models/CallableClassType.php
@@ -0,0 +1,15 @@
+<?php
+namespace PHP\Types\Models;
+
+/**
+ * Defines a class that can be invoked like a function
+ */
+class CallableClassType extends Type
+{
+
+    
+    public function __construct( string $name, array $aliases = [] )
+    {
+
+    }
+}
