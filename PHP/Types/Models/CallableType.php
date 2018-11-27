b03bb0bb793535b90b189250640bd1a56a3f8106
EvanWashkow <epwashkow@gmail.com>
2018-11-27 10:01:48 -0800

    Document callable type

--- PHP/Types/Models/CallableType.php
+++ PHP/Types/Models/CallableType.php
@@ -3,5 +3,9 @@ namespace PHP\Types\Models;
 
 /**
  * Defines an interface for callable types
+ * 
+ * Callable types:
+ * 1. Provide a shortcut to a function
+ * 2. Provide a function-like interface to invoke said function
  */
 interface CallableType {}
