75268b780c32e3e96e3559a0d9551a137121624f
EvanWashkow <epwashkow@gmail.com>
2018-12-15 14:20:11 -0800

    Rename section break

--- tests/Collections/CollectionTest.php
+++ tests/Collections/CollectionTest.php
@@ -897,7 +897,7 @@ class CollectionTest extends CollectionsTestCase
 
 
     /***************************************************************************
-    *                            Collection->getKeys()
+    *                           Collection Test Data
     ***************************************************************************/
 
 
