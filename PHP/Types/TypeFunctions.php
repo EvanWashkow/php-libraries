3c071ba5ca48247aa7e46bfd0bddb8ca04b642bf
Evan Washkow <epwashkow@gmail.com>
2019-01-12 11:27:16 -0800

    Rewrite is() to make lower level calls

--- PHP/Types/TypeFunctions.php
+++ PHP/Types/TypeFunctions.php
@@ -1,14 +1,54 @@
 <?php
 declare( strict_types = 1 );
 
+use PHP\Types\TypeNames;
+
 /**
- * Determine if the variable is, or is derived from, the type
+ * Determine if the value is, or is derived from, the type
  *
- * @param mixed  $variable The variable
- * @param string $type     Type string to compare the variable's type with
+ * @param mixed  $value The value
+ * @param string $type  Type name to compare the value to
  * @return bool
  */
-function is( $variable, string $type ): bool
+function is( $value, string $type ): bool
 {
-    return \PHP\Types::GetByValue( $variable )->is( $type );
+    $result;
+    switch ( $type ) {
+        case TypeNames::ARRAY:
+            $result = is_array( $value );
+            break;
+
+        case TypeNames::BOOL:
+        case TypeNames::BOOLEAN:
+            $result = is_bool( $value );
+            break;
+
+        case TypeNames::DOUBLE:
+        case TypeNames::FLOAT:
+            $result = is_float( $value );
+            break;
+
+        case TypeNames::INT:
+        case TypeNames::INTEGER:
+            $result = is_int( $value );
+            break;
+
+        case TypeNames::NULL:
+            $result = ( NULL === $value );
+            break;
+
+        case TypeNames::STRING:
+            $result = is_string( $value );
+            break;
+
+        default:
+            if ( class_exists( $type ) || interface_exists( $type )) {
+                $result = is_a( $value, $type );
+            }
+            else {
+                $result = false;
+            }
+            break;
+    }
+    return $result;
 }
