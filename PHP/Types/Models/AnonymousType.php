37727a1d2bbe692820e967f6dd588e6dd5ffa6f5
Evan Washkow <epwashkow@gmail.com>
2019-01-14 15:06:08 -0800

    Document the crap out of AnonymousType

--- PHP/Types/Models/AnonymousType.php
+++ PHP/Types/Models/AnonymousType.php
@@ -9,13 +9,22 @@ use PHP\Types\Models\Type;
  * A "typeless type", where the compiler/interpreter has to infer the type from
  * the value, at run-time.
  * 
- * @internal Name cannot be a valid type or possible function name. Therefore,
- * the name must be one that PHP does not allow. Also, '*' is pretty
+ * @internal Name cannot be a valid type name or possible function name.
+ * Therefore, the name must be one that PHP does not allow. Also, '*' is pretty
  * descriptive of an anonymous type.
  * 
  * @internal Do not use as the base class for Type, even though PHP's default
  * behavior is anonymous typing. Under the hood, an anonymous type is
  * implemented using type inference, which uses strict typing.
+ * 
+ * @internal This is not available via Type lookup methods, for several reasons:
+ * 1. It's not an actual type. Rather, it's a type container for actual types,
+ * which, when queried, results in a non-anonymous type.
+ * 2. Strict typing is much preferred: its more secure and faster.
+ * 3. However, it is useful when defining generics, like Collections.
+ * 
+ * @todo Once PHP defines a type name for anonymous types, modify this class
+ * to use that name.
  */
 class AnonymousType extends Type
 {
