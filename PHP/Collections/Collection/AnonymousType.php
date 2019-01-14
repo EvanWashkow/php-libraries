ee47128211f01f3e65284508d534ed4fb4fe807a
Evan Washkow <epwashkow@gmail.com>
2019-01-14 13:59:24 -0800

    Redocument anonymous type

--- PHP/Collections/Collection/AnonymousType.php
+++ PHP/Collections/Collection/AnonymousType.php
@@ -6,10 +6,12 @@ namespace PHP\Collections\Collection;
 use PHP\Types\Models\Type;
 
 /**
- * Type that evaluates to true for any type
+ * A "typeless type", where the compiler/interpreter has to infer the type from
+ * the value, at run-time.
  * 
- * @internal This should not be the base type for Type, since, under the hood,
- * an anonymous type is implemented using various types.
+ * @internal Do not use as the base class for Type, even though PHP's default
+ * behavior is anonymous typing. Under the hood, an anonymous type is
+ * implemented using type inference, which uses strict typing.
  */
 class AnonymousType extends Type
 {
