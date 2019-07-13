ba4d2e716c7157a78312eda34876d33181f8c57e
Evan Washkow <epwashkow@gmail.com>
2019-07-13 11:11:15 -0700

    Deprecate PHPObject

--- PHP/IPHPObject.php
+++ PHP/IPHPObject.php
@@ -1,6 +1,8 @@
 <?php
 namespace PHP;
 
+trigger_error( 'PHP\\IPHPObject is deprecated', E_USER_DEPRECATED );
+
 /**
  * Specifications for an PHPObject
  */
--- PHP/PHPObject.php
+++ PHP/PHPObject.php
@@ -1,6 +1,9 @@
 <?php
 namespace PHP;
 
+trigger_error( 'PHP\\PHPObject is deprecated', E_USER_DEPRECATED );
+
+
 /**
  * Base definition for all PHPObject instances
  */
