544c5f6e981a7db80937353701692e6204a61352
Evan Washkow <epwashkow@gmail.com>
2019-07-27 22:39:01 -0700

    Document ClassType final methods

--- PHP/Types/Models/ClassType.php
+++ PHP/Types/Models/ClassType.php
@@ -8,14 +8,20 @@ namespace PHP\Types\Models;
  */
 final class ClassType extends InterfaceType
 {
-    
-    
+
+
+    /**
+     * @internal Final: this class will always be a class.
+     */
     final public function isClass(): bool
     {
         return true;
     }
-    
-    
+
+
+    /**
+     * @internal Final: this class can never be an interface.
+     */
     final public function isInterface(): bool
     {
         return false;
