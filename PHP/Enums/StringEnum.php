8362176b507b538741e6259ab79d348ed65e5f3b
Evan Washkow <epwashkow@gmail.com>
2019-07-31 22:44:19 -0700

    Document string enum

--- PHP/Enums/StringEnum.php
+++ PHP/Enums/StringEnum.php
@@ -14,10 +14,13 @@ abstract class StringEnum extends Enum
     /**
      * Modify Constants to only support strings
      * 
+     * @internal Final: it is a strict requirement that all constants in a
+     * String Enumeration should be strings.
+     * 
      * @param array $constants This class's array of constants
      * @throws \DomainException On non-string constant
      */
-    protected function __constructConstantsDictionary( array $constants )
+    final protected function __constructConstantsDictionary( array $constants )
     {
         $dictionary = new Dictionary( 'string', 'string' );
         foreach ( $constants as $key => $value ) {
@@ -28,5 +31,27 @@ abstract class StringEnum extends Enum
             $dictionary->set( $key, $value );
         }
         return $dictionary;
+    }
+
+
+    public function getValue(): string
+    {
+        return parent::getValue();
+    }
+
+
+    /**
+     * Set the current value
+     * 
+     * @param string $value A value from the set of enumerated constants
+     * @throws \InvalidArgumentException If the value is not a string
+     * @throws \DomainException If the value is not in the set of enumerated constants
+     */
+    protected function setValue( $value )
+    {
+        if ( !is_string( $value )) {
+            throw new \InvalidArgumentException( 'Given value was not a string.' );
+        }
+        parent::setValue( $value );
     }
 }
