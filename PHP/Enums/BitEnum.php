14fac6e3a2b97dfebdc6ce76092ad9d9eda92150
Evan Washkow <epwashkow@gmail.com>
2019-08-10 18:22:47 -0700

    Make BitEnum abstract

--- PHP/Enums/BitEnum.php
+++ PHP/Enums/BitEnum.php
@@ -7,7 +7,7 @@ namespace PHP\Enums;
  * Defines a strict set of bit flags, allowing the user to specify one or more
  * of them in a bitwise OR (|) operation.
  */
-class BitEnum extends IntegerEnum
+abstract class BitEnum extends IntegerEnum
 {
     
 }
