600c95634860011212b2296b87903ac418395e5e
Evan Washkow <epwashkow@gmail.com>
2019-08-18 08:46:07 -0700

    Test DomainException

--- tests/Enums/EnumInfo/EnumInfoTest.php
+++ tests/Enums/EnumInfo/EnumInfoTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace PHP\Tests\Enums\EnumInfo;
 
 use PHP\Enums\EnumInfo\EnumInfo;
+use PHP\ObjectClass;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -21,5 +22,16 @@ class EnumInfoTest extends TestCase
     public function testConstructThrowsNotFoundException()
     {
         new EnumInfo( 'foobar' );
+    }
+
+
+    /**
+     * Test __construct() throws DomainException
+     * 
+     * @expectedException \DomainException
+     */
+    public function testConstructThrowsDomainException()
+    {
+        new EnumInfo( ObjectClass::class );
     }
 }
