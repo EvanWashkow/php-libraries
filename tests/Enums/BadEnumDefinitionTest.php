cc61b15cf2ab4babbd470ee5d454bfd136e5aaad
Evan Washkow <epwashkow@gmail.com>
2019-11-02 13:33:20 -0700

    Write test for Bad Enum definitions

---
+++ tests/Enums/BadEnumDefinitionTest.php
@@ -0,0 +1,41 @@
+<?php
+declare( strict_types = 1 );
+
+namespace PHP\Tests\Enums;
+
+use PHP\Tests\Enums\TestEnumDefinitions\BadIntegerEnum;
+use PHP\Tests\Enums\TestEnumDefinitions\BadStringEnum;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Tests the construction / fetching of bad Enum definitions to ensure they
+ * throw exceptions
+ */
+class BadEnumDefinitionTest extends TestCase
+{
+
+    /**
+     * Test the construction / fetching of bad Enum definitions to ensure they
+     * throw exceptions
+     * 
+     * @expectedException \DomainException
+     * @dataProvider getData()
+     */
+    public function test( \Closure $callback )
+    {
+        $callback();
+    }
+
+
+    public function getData(): array
+    {
+        return [
+            'BadIntegerEnum' => [
+                function() { new BadIntegerEnum( BadIntegerEnum::NUMBERS ); }
+            ],
+            'BadStringEnum' => [
+                function() { new BadStringEnum( BadStringEnum::A ); }
+            ],
+        ];
+    }
+}
