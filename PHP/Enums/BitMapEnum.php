ae0d39d561137c5a96f660c7d6eeec3e547fa81f
Evan Washkow <epwashkow@gmail.com>
2019-11-20 21:56:26 -0800

    Rename BitEnum => BitMapEnum

---
+++ PHP/Enums/BitMapEnum.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace PHP\Enums;
+
+/**
+ * Defines a set of bit maps, allowing the user to specify one or more of them in a bitwise OR (|) operation.
+ */
+abstract class BitMapEnum extends IntegerEnum
+{
+    
+}
