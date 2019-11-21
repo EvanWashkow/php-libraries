f23a264eb0d40c772fdb732c26c3d652a2b5c589
Evan Washkow <epwashkow@gmail.com>
2019-11-20 22:14:15 -0800

    Redocument Malformed Enum Exception

--- PHP/Enums/Exceptions/MalformedEnumException.php
+++ PHP/Enums/Exceptions/MalformedEnumException.php
@@ -4,8 +4,8 @@ declare( strict_types = 1 );
 namespace PHP\Enums\Exceptions;
 
 /**
- * Exception thrown when an Enum's definition does not adhear to its type.
+ * Exception thrown when an Enum child class defines constants prohibited by the parent Enum type.
  * 
- * For example, but not limited to, a String Enum that attempts to define Integer constants.
+ * For example, when a String Enum attemps to define Integer constants.
  */
 class MalformedEnumException extends \DomainException {}
