1fa36472ddd693842b32d62cb2ba91064181305b
Evan Washkow <epwashkow@gmail.com>
2020-01-15 22:07:12 -0800

    Define Private Constant Enum

---
+++ tests/Enums/TestEnumDefinitions/PrivateConstantEnum.php
@@ -0,0 +1,13 @@
+<?php
+declare( strict_types = 1 );
+
+namespace PHP\Tests\Enums\TestEnumDefinitions;
+
+use PHP\Enums\Enum;
+
+class PrivateConstantEnum extends Enum
+{
+    public const PUBLIC = 'public';
+
+    private const PRIVATE = 'private';
+}
