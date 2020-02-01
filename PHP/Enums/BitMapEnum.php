78c0b54fad1c9cde7c08b94ab3722e9756ef6190
Evan Washkow <epwashkow@gmail.com>
2020-02-01 10:56:58 -0800

    BitMapEnum->isSet() throws InvalidArgumentException on wrong arg type

--- PHP/Enums/BitMapEnum.php
+++ PHP/Enums/BitMapEnum.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace PHP\Enums;
 
+use PHP\Types;
+
 /**
  * Defines a set of bit maps, allowing the user to specify one or more of them in a bitwise OR (|) operation.
  * 
@@ -46,6 +48,13 @@ abstract class BitMapEnum extends IntegerEnum
      */
     public function isSet( $bitMap ): bool
     {
+        if ( $bitMap instanceof BitMapEnum ) {
+            $bitMap = $bitMap->getValue();
+        }
+        if ( !is_int( $bitMap )) {
+            $type = Types::GetByValue( $bitMap );
+            throw new \InvalidArgumentException( "BitMapEnum->isSet() expects an Integer or BitMapEnum. {$type->getName()} given." );
+        }
         return $this->isSubset( $this->getValue(), $bitMap );
     }
 
