87ca2653b9ffa3d9353d940ce572e2f9622d5ed4
Evan Washkow <epwashkow@gmail.com>
2020-03-24 22:40:33 -0700

    Declare all abstract Iterator methods and their documentation

--- src/Loops/Iterator.php
+++ src/Loops/Iterator.php
@@ -17,6 +17,44 @@ abstract class Iterator implements \Iterator
     abstract public function rewind(): void;
 
 
+    /**
+     * Determines if there is a current item (with a key and value)
+     * 
+     * @return bool
+     */
+    abstract public function hasCurrent(): bool;
+
+
+    /**
+     * Retrieve the key for the current item
+     * 
+     * @return scalar
+     * @throws \OutOfBoundsException If the current position is invalid
+     */
+    abstract public function getKey();
+
+
+    /**
+     * Retrieve the value for the current item
+     * 
+     * @return mixed
+     * @throws \OutOfBoundsException If the current position is invalid
+     */
+    abstract public function getValue();
+
+
+    /**
+     * Proceed to the next item (regardless if it exists or not)
+     * 
+     * @internal This is one of the inherit design flaws of PHP: you must increment to an invalid position and then
+     * report it as such. This is why getKey() and getValue() throw \OutOfBoundExceptions, and why this interim class
+     * was added. Unfortunately, this design problem cannot be solved.
+     * 
+     * @return void
+     */
+    abstract public function goToNext(): void;
+
+
     final public function current()
     {
         throw new NotImplementedException( __FUNCTION__ . '() is not implemented, yet.' );
