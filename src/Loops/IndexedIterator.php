217a8c7153634d59ba7d7df73936aa54844664b1
Evan Washkow <epwashkow@gmail.com>
2020-03-28 11:45:04 -0700

    Stub IndexedIterator

---
+++ src/Loops/IndexedIterator.php
@@ -0,0 +1,9 @@
+<?php
+declare( strict_types = 1 );
+
+namespace PHP\Loops;
+
+/**
+ * Uses integer indices to traverse an Iterable item in a foreach() loop, retrieving the item's value for that index.
+ */
+class IndexedIterator {}
