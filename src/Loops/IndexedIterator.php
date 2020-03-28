85941efd508b71222011cd91c83b4e057563a4f9
Evan Washkow <epwashkow@gmail.com>
2020-03-28 16:01:31 -0700

    Define indexed iterator constructor with more standard arguments

--- src/Loops/IndexedIterator.php
+++ src/Loops/IndexedIterator.php
@@ -17,17 +17,13 @@ abstract class IndexedIterator extends Iterator
     /**
      * Create a new integer-indexed iterator to traverse an Iterable
      * 
-     * @internal Using "count" rather than "endingIndex" for two primary reasons:
-     * 1. "endingIndex" cannot ever indicate "there are no values." i.e. start = 0, end = 0 means one value. Arguing
-     * that "no values" should be represented by end = ( start - 1 ) complicates point #2.
-     * 2. If the direction is reversed, "endingIndex" must be inverted to the negative. This defeats the purpose of the
-     * bi-directional "incrementBy" argument, and makes switching directions much more difficult.
+     * In order to specify no items, specify an invalid ending index. i.e. start = 0; end = -1; increment = 1.
      * 
-     * @param int $startingIndex The index to start at.
-     * @param int $count         How many items to iterate over.
-     * @param int $incrementBy   Value to increment (or decrement) the index by (on goToNext()).
+     * @param int $start     The index to start at.
+     * @param int $end       The index to end at. Will return the value at that index.
+     * @param int $increment Value to increment (or decrement) the index by (on goToNext()).
      */
-    public function __construct( int $startingIndex, int $count, int $incrementBy = 1 )
+    public function __construct( int $start, int $end, int $increment = 1 )
     {
         return;
     }
