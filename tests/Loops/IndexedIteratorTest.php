2ff1ba343c98ad364210f2f4a5ee1a3ebac647b4
Evan Washkow <epwashkow@gmail.com>
2020-03-29 13:49:00 -0700

    Write test for goToNext()

--- tests/Loops/IndexedIteratorTest.php
+++ tests/Loops/IndexedIteratorTest.php
@@ -159,9 +159,63 @@ class IndexedIteratorTest extends TestCase
 
     /*******************************************************************************************************************
     *                                                     goToNext()
-    *
-    * This method is tested in due course of testing hasCurrent() and getKey()
     *******************************************************************************************************************/
+
+
+    /**
+     * Ensure goToNext() retrieves the expected keys
+     * 
+     * @dataProvider getGoToNextData
+     */
+    public function testGoToNext( IndexedIterator $iterator, array $expectedKeys )
+    {
+        $actualKeys = [];
+        $iterator->rewind();
+        while ( $iterator->hasCurrent() ) {
+            $actualKeys[] = $iterator->getKey();
+            $iterator->goToNext();
+        }
+
+        $this->assertEquals(
+            $expectedKeys,
+            $actualKeys,
+            'IndexedIterator->goToNext() did not correctly proceed to the next value.'
+        );
+    }
+
+    public function getGoToNextData(): array
+    {
+        return [
+
+            // Forwards
+            'start = 24, end = 35, increment = 1' => [
+                $this->createIndexedIterator( 24, 35, 1 ),
+                [ 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35 ]
+            ],
+            'start = 7, end = 28, increment = 2' => [
+                $this->createIndexedIterator( 7, 28, 2 ),
+                [ 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27 ]
+            ],
+            'start = 7, end = 23, increment = 3' => [
+                $this->createIndexedIterator( 7, 23, 3 ),
+                [ 7, 10, 13, 16, 19, 22 ]
+            ],
+
+            // Backwards
+            'start = -5, end = -13, increment = -1' => [
+                $this->createIndexedIterator( -5, -13, -1 ),
+                [ -5, -6, -7, -8, -9, -10, -11, -12, -13 ]
+            ],
+            'start = -1, end = -13, increment = -2' => [
+                $this->createIndexedIterator( -1, -13, -2 ),
+                [ -1, -3, -5, -7, -9, -11, -13 ]
+            ],
+            'start = -7, end = -15, increment -3' => [
+                $this->createIndexedIterator( -7, -25, -3 ),
+                [ -7, -10, -13, -16, -19, -22, -25 ]
+            ]
+        ];
+    }
 
 
 
