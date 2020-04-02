185c15dbe9267d292e4dcb9c9d358c93fcea950b
Evan Washkow <epwashkow@gmail.com>
2020-04-01 23:05:37 -0700

    Stub KeyValuePair class

---
+++ src/Collections/KeyValuePair.php
@@ -0,0 +1,21 @@
+<?php
+namespace PHP\Collections;
+
+/**
+ * Defines a key-value pair that can be retrieved
+ */
+class KeyValuePair
+{
+
+
+    /**
+     * Create a new key-value pair
+     *
+     * @param mixed $key   The key
+     * @param mixed $value The value
+     **/
+    public function __construct( $key, $value )
+    {
+        return;
+    }
+}
