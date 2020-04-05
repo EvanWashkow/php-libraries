0c12e10fc008adf7582d55134f632e94e427af12
Evan Washkow <epwashkow@gmail.com>
2020-04-05 14:34:34 -0700

    Remove unreferenced use clause

--- src/Collections/Iterators/SequenceIterator.php
+++ src/Collections/Iterators/SequenceIterator.php
@@ -4,7 +4,6 @@ declare( strict_types = 1 );
 namespace PHP\Collections\Iterators;
 
 use PHP\Collections\Sequence;
-use PHP\Exceptions\NotImplementedException;
 use PHP\Iteration\IndexedIterator;
 
 /**
