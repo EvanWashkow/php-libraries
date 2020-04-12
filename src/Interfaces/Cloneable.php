979fab886bf20cbdcfec01ba332312a7afaedbe8
Evan Washkow <epwashkow@gmail.com>
2020-04-11 23:18:46 -0700

    Redocument Cloneable

--- src/Interfaces/Cloneable.php
+++ src/Interfaces/Cloneable.php
@@ -4,11 +4,12 @@ declare( strict_types = 1 );
 namespace PHP\Interfaces;
 
 /**
- * Describes any object that can be duplicated
+ * Describes an object that can be duplicated
  * 
- * @internal All objects can be cloned via "clone $x", however, the syntax here
- * is a more object-oriented syntax. Additionally, the magic "__clone" method
- * is called after cloning, but this is called before.
+ * @internal PHP makes the assumption that all objects can be cloned (via `clone $x`). This is an incorrect assumption
+ * to make since not every implementation wants to allow it. This interface provides an object-oriented way to implement
+ * and indicate clonability on a class-by-class basis. Additionally, this method is called before cloning happens,
+ * whereas the magic "__clone" method is called after, which gives far more control.
  */
 interface Cloneable
 {
