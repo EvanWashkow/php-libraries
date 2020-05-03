aec273b38b0d70c205a2afa848f32f7b93cd3d70
Evan Washkow <epwashkow@gmail.com>
2020-05-02 23:15:43 -0700

    Implement ArraybleIndexedIterator->toArray()

--- src/Collections/Iteration/ArrayableIndexedIterator.php
+++ src/Collections/Iteration/ArrayableIndexedIterator.php
@@ -7,12 +7,15 @@ use PHP\Collections\IArrayable;
 use PHP\Exceptions\NotImplementedException;
 
 /**
- * Iterates over an IArrayable using Integer indexes from 0 to n-1.
+ * Iterates over an IArrayable using Integer indexes.
  * 
  * Changes to the object will be reflected in the loop. Clone IArrayable entries before looping as necessary.
  */
 class ArrayableIndexedIterator extends IndexedIterator
 {
+
+    /** @var IArrayable $arrayable The IArrayable object instance */
+    private $arrayable;
 
 
     /**
@@ -25,11 +28,15 @@ class ArrayableIndexedIterator extends IndexedIterator
      */
     public function __construct( IArrayable $arrayable, int $startingIndex = 0, int $incrementBy = 1 )
     {
+        // Call parent constructor
         try {
             parent::__construct( $startingIndex, $incrementBy );
         } catch ( \DomainException $de ) {
             throw new \DomainException( $de->getMessage(), $de->getCode(), $de );
         }
+
+        // Set own properties
+        $this->arrayable = $arrayable;
     }
 
 
@@ -42,5 +49,18 @@ class ArrayableIndexedIterator extends IndexedIterator
     public function getValue()
     {
         throw new NotImplementedException( 'Not implemented, yet.' );
+    }
+
+
+    /**
+     * Retrieve the IArrayable array with integer indexes
+     * 
+     * This will fetch the current values from IArrayable, which will reflect any changes (additions / removals) there.
+     * 
+     * @return array
+     */
+    protected function toArray(): array
+    {
+        return array_values( $this->arrayable->toArray() );
     }
 }
