d9bce55d8b88232976688fdb674bd9b7cf8d2547
Evan Washkow <epwashkow@gmail.com>
2020-08-13 22:23:02 -0700

    Tweak IHashable's class documentation

--- src/Hashing/IHashable.php
+++ src/Hashing/IHashable.php
@@ -6,7 +6,7 @@ namespace PHP\Hashing;
 use PHP\Collections\ByteArray;
 
 /**
- * Describes an Object that can provides its own Hash sum
+ * Describes an Object that provides its own Hash sum
  * 
  * When paired with IEquatable, the results should be equivalent. If x.equals(y) returns true, then the value returned
  * by the x.hash() should be equal to y.hash().
