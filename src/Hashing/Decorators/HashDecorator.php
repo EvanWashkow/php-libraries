d1896b02b8a7f6ca142385a10838e0f23971d097
Evan Washkow <epwashkow@gmail.com>
2020-08-19 23:07:20 -0700

    Tighten the documentation on Hash Decorator

--- src/Hashing/Decorators/HashDecorator.php
+++ src/Hashing/Decorators/HashDecorator.php
@@ -4,8 +4,6 @@ declare( strict_types = 1 );
 namespace PHP\Hashing\Decorators;
 
 /**
- * Customizes how a hash is generated for **any** given value.
- * 
  * Allows the user to customize how a hash is generated for any given value by chaining together a series of
  * instructions using the Decorator Pattern.
  */
