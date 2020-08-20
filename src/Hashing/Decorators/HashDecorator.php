d2c24a626a20c7b5d6ed239d8a6b71317097641c
Evan Washkow <epwashkow@gmail.com>
2020-08-19 23:03:07 -0700

    Stub the Hash Decorator

---
+++ src/Hashing/Decorators/HashDecorator.php
@@ -0,0 +1,15 @@
+<?php
+declare( strict_types = 1 );
+
+namespace PHP\Hashing\Decorators;
+
+/**
+ * Customizes how a hash is generated for **any** given value.
+ * 
+ * Allows the user to customize how a hash is generated for any given value by chaining together a series of
+ * instructions using the Decorator Pattern.
+ */
+abstract class HashDecorator
+{
+
+}
