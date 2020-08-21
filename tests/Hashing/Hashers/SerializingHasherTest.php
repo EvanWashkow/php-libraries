12c8dc9356011b73d888e469c12688c2561f2e21
Evan Washkow <epwashkow@gmail.com>
2020-08-20 22:15:45 -0700

    Add factory functions to create Hash Algorithm and Serializer

--- tests/Hashing/Hashers/SerializingHasherTest.php
+++ tests/Hashing/Hashers/SerializingHasherTest.php
@@ -3,10 +3,11 @@ declare( strict_types = 1 );
 
 namespace PHP\Hashing\Hasher;
 
-use PHP\Hashing\HashAlgorithms\MD5;
+use PHP\Collections\ByteArray;
+use PHP\Hashing\HashAlgorithms\IHashAlgorithm;
 use PHP\Hashing\Hashers\IHasher;
 use PHP\Hashing\Hashers\SerializingHasher;
-use PHP\Serialization\PHPSerializer;
+use PHP\Serialization\ISerializer;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -23,8 +24,39 @@ class SerializingHasherTest extends TestCase
     {
         $this->assertInstanceOf(
             IHasher::class,
-            new SerializingHasher( new PHPSerializer(), new MD5() ),
+            new SerializingHasher(
+                $this->createSerializer( function() { return ''; } ),
+                $this->createHashAlgorithm( function() { return new ByteArray( '' ); } )
+            ),
             'SerializingHasher is not an instance of IHasher.'
         );
+    }
+
+
+    /**
+     * Create a HashAlgorithm instance
+     * 
+     * @param \Closure $hash The hash() function callback
+     * @return IHashAlgorithm
+     */
+    private function createHashAlgorithm( \Closure $hash ): IHashAlgorithm
+    {
+        $hashAlgorithm = $this->createMock( IHashAlgorithm::class );
+        $hashAlgorithm->method( 'hash' )->willReturnCallback( $hash );
+        return $hashAlgorithm;
+    }
+
+
+    /**
+     * Create a Serializer instance
+     * 
+     * @param \Closure $serialize The serialize() function callback
+     * @return ISerializer
+     */
+    private function createSerializer( \Closure $serialize ): ISerializer
+    {
+        $serializer = $this->createMock( ISerializer::class );
+        $serializer->method( 'serialize' )->willReturnCallback( $serialize );
+        return $serializer;
     }
 }
