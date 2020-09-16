c6b97efd0f1dc96347f606120f225463d54dbc92
Evan Washkow <epwashkow@gmail.com>
2020-09-15 20:18:40 -0700

    Create HasherDecorator class implementation

--- src/Hashing/Hashers/HasherDecorator.php
+++ src/Hashing/Hashers/HasherDecorator.php
@@ -3,11 +3,33 @@ declare(strict_types=1);
 
 namespace PHP\Hashing\Hashers;
 
-use PHP\Collections\ByteArray;
-
 /**
  * Modifies hash results using the Decorator pattern
  */
 abstract class HasherDecorator implements IHasher
 {
+
+    /** @var IHasher $nextHasher The next Hasher in the sequence to execute */
+    private $nextHasher;
+
+
+    /**
+     * Create a new Hasher Decorator
+     * @param IHasher $nextHasher The next Hasher in the sequence to execute
+     */
+    public function __construct(IHasher $nextHasher)
+    {
+        $this->nextHasher = $nextHasher;
+    }
+
+
+    /**
+     * Retrieve the next IHasher in the execution sequence
+     * @internal Final as this should always return the value from the constructor.
+     * @return IHasher
+     */
+    final protected function getNextHasher(): IHasher
+    {
+        return $this->nextHasher;
+    }
 }
--- tests/Hashing/Hashers/HasherDecoratorTest.php
+++ tests/Hashing/Hashers/HasherDecoratorTest.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace PHP\Tests\Hashing\Hashers;
 
+use PHP\Collections\ByteArray;
+use PHP\Exceptions\NotImplementedException;
 use PHP\Hashing\Hashers\HasherDecorator;
 use PHP\Hashing\Hashers\IHasher;
 
@@ -23,5 +25,67 @@ class HasherDecoratorTest extends \PHPUnit\Framework\TestCase
             $this->createMock(HasherDecorator::class),
             'HasherDecorator not an instance of IHasher.'
         );
+    }
+
+
+    /**
+     * Test __construct() and getNextHasher()
+     * @param IHasher $hasher
+     * @dataProvider getConstructorAndGetNextTestData
+     */
+    public function testConstructorAndGetNext(IHasher $hasher): void
+    {
+        // Create Hasher Decorator instance to test the __construct() and getNextHasher()
+        $hasherDecorator = new class($hasher) extends HasherDecorator
+        {
+            public function hash($value): ByteArray
+            {
+                throw new NotImplementedException('Not implemented.');
+            }
+
+            public function getNextHasherTest(): IHasher
+            {
+                return parent::getNextHasher();
+            }
+        };
+
+        $this->assertEquals(
+            $hasher,
+            $hasherDecorator->getNextHasherTest(),
+            'HasherDecorator->getNextHasher() does not return the expected IHasher instance.'
+        );
+    }
+
+    public function getConstructorAndGetNextTestData(): array
+    {
+        return [
+            'Hasher that returns ByteArray(1, 1)' => [
+                new class implements IHasher
+                {
+                    public function hash($value): ByteArray
+                    {
+                        return new ByteArray(1, 1);
+                    }
+                }
+            ],
+            'Hasher that returns ByteArray(2, 1)' => [
+                new class implements IHasher
+                {
+                    public function hash($value): ByteArray
+                    {
+                        return new ByteArray(2, 1);
+                    }
+                }
+            ],
+            'Hasher that returns ByteArray(3, 1)' => [
+                new class implements IHasher
+                {
+                    public function hash($value): ByteArray
+                    {
+                        return new ByteArray(3, 1);
+                    }
+                }
+            ]
+        ];
     }
 }
