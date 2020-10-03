9535df7ed5cf35f39e3a5df68189961cb54524e8
Evan Washkow <epwashkow@gmail.com>
2020-10-02 22:32:13 -0700

    Fix old test references to IHasher

--- tests/Collections/ByteArrayConverter/PrimitiveValueByteArrayConverterTest.php
+++ tests/Collections/ByteArrayConverter/PrimitiveValueByteArrayConverterTest.php
@@ -49,21 +49,21 @@ class PrimitiveValueByteArrayConverterTest extends \PHPUnit\Framework\TestCase
         $converterFactory = new ByteArrayConverterFactory();
         $mockConverter    = $this->createMock(IByteArrayConverter::class);
         return [
-            'hash(1)'        => [ $mockConverter, 1,        new ByteArray(1)],
-            'hash(1.1)'      => [ $mockConverter, 1.1,      new ByteArray(1.1)],
-            'hash("string")' => [ $mockConverter, 'string', new ByteArray('string')],
+            'convert(1)'        => [ $mockConverter, 1,        new ByteArray(1)],
+            'convert(1.1)'      => [ $mockConverter, 1.1,      new ByteArray(1.1)],
+            'convert("string")' => [ $mockConverter, 'string', new ByteArray('string')],
 
-            'hash(non-primitive) returns ByteArray(1)' => [
+            'convert(non-primitive) returns ByteArray(1)' => [
                 $converterFactory->convertReturns(new ByteArray(1)),
                 new class {},
                 new ByteArray(1)
             ],
-            'hash(non-primitive) returns ByteArray(2)' => [
+            'convert(non-primitive) returns ByteArray(2)' => [
                 $converterFactory->convertReturns(new ByteArray(2)),
                 new class {},
                 new ByteArray(2)
             ],
-            'hash(non-primitive) returns ByteArray(3)' => [
+            'convert(non-primitive) returns ByteArray(3)' => [
                 $converterFactory->convertReturns(new ByteArray(3)),
                 new class {},
                 new ByteArray(3)
