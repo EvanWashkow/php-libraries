bb14366789d1cf41f54322207f2dad7c336ba6fa
Evan Washkow <epwashkow@gmail.com>
2020-12-26 13:23:48 -0800

    Alphebetize IEquatable methods

--- src/Interfaces/IEquatable.php
+++ src/Interfaces/IEquatable.php
@@ -11,6 +11,30 @@ use PHP\Collections\ByteArray;
 interface IEquatable
 {
 
+
+    /**
+     * Retrieves the hash sum for this Object.
+     *
+     * Rules:
+     *
+     * Hashes serve as an Object's fingerprint. Thus, they must not change over the Object's lifetime. If an Object is
+     * contained in a Collection, any change to its hash would cause it to be lost in the Collection's hash table.
+     *
+     * If x.equals(y) returns true, then x.hash() must equal y.hash(). However, the inverse is not necessarily true.
+     * If x.hash() equals y.hash(), this does not mean that x.equals(y). Hashes are not guaranteed to be unique;
+     * however, the more unique they are, the faster they can be retrieved from Collections.
+     *
+     * @internal
+     * Suggestions for Implementers:
+     *
+     * Either compute the hash from immutable fields, or, if there are none, return a new Object with the change.
+     * See equals().
+     *
+     * @return ByteArray
+     */
+    public function hash(): ByteArray;
+
+
     /**
      * Determines if this Object is equal to another value
      * 
@@ -25,27 +49,4 @@ interface IEquatable
      * @return bool
      */
     public function equals( $value ): bool;
-
-
-    /**
-     * Retrieves the hash sum for this Object.
-     * 
-     * Rules:
-     * 
-     * Hashes serve as an Object's fingerprint. Thus, they must not change over the Object's lifetime. If an Object is
-     * contained in a Collection, any change to its hash would cause it to be lost in the Collection's hash table.
-     * 
-     * If x.equals(y) returns true, then x.hash() must equal y.hash(). However, the inverse is not necessarily true.
-     * If x.hash() equals y.hash(), this does not mean that x.equals(y). Hashes are not guaranteed to be unique;
-     * however, the more unique they are, the faster they can be retrieved from Collections.
-     * 
-     * @internal
-     * Suggestions for Implementers:
-     * 
-     * Either compute the hash from immutable fields, or, if there are none, return a new Object with the change.
-     * See equals().
-     * 
-     * @return ByteArray
-     */
-    public function hash(): ByteArray;
 }
