2a1936f3b642d521010934ee62be44bd97b008e9
Evan Washkow <epwashkow@gmail.com>
2021-01-02 14:41:36 -0800

    Convert ObjectClassTest to use IEquatableTests

--- tests/ObjectClassTest.php
+++ tests/ObjectClassTest.php
@@ -3,8 +3,9 @@ declare( strict_types = 1 );
 
 namespace PHP\Tests;
 
+use PHP\Collections\ByteArray;
 use PHP\ObjectClass;
-use PHP\Tests\Interfaces\IEquatableTestTrait;
+use PHP\Tests\Interfaces\IEquatableTests;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -13,11 +14,20 @@ use PHPUnit\Framework\TestCase;
 class ObjectClassTest extends TestCase
 {
 
-    /**
-     * Define test data for IEquatableTestTrait
-     */
-    use IEquatableTestTrait;
 
+    /**
+     * Test equals() return value
+     *
+     * @dataProvider getEqualsTestData
+     *
+     * @param ObjectClass $objectClass
+     * @param $value
+     * @param bool $expected
+     */
+    public function testEquals(ObjectClass $objectClass, $value, bool $expected): void
+    {
+        $this->getIEquatableTests()->testEquals($objectClass, $value, $expected);
+    }
 
     public function getEqualsTestData(): array
     {
@@ -34,6 +44,20 @@ class ObjectClassTest extends TestCase
         ];
     }
 
+
+    /**
+     * Test hash() return value
+     *
+     * @dataProvider getHashTestData
+     *
+     * @param ObjectClass $objectClass
+     * @param ByteArray $byteArray
+     * @param bool $expected
+     */
+    public function testHash(ObjectClass $objectClass, ByteArray $byteArray, bool $expected): void
+    {
+        $this->getIEquatableTests()->testHash($objectClass, $byteArray, $expected);
+    }
 
     public function getHashTestData(): array
     {
@@ -57,12 +81,40 @@ class ObjectClassTest extends TestCase
     }
 
 
+    /**
+     * Ensure hash() and equals() behave consistently
+     *
+     * @dataProvider getEqualsAndHashConsistencyTestData
+     *
+     * @param ObjectClass $objectClass1
+     * @param ObjectClass $objectClass2
+     */
+    public function testEqualsAndHashConsistency(ObjectClass $objectClass1, ObjectClass $objectClass2): void
+    {
+        $this->getIEquatableTests()->testEqualsAndHashConsistency($objectClass1, $objectClass2);
+    }
+
     public function getEqualsAndHashConsistencyTestData(): array
     {
         $o1 = $this->createObjectClass();
         return [
             'o1' => [ $o1, $o1 ]
         ];
+    }
+
+
+    /**
+     * Retrieve IEquatable Tests for this Test Case
+     * @return IEquatableTests
+     */
+    private function getIEquatableTests(): IEquatableTests
+    {
+        static $iequatableTests = null;
+        if (null === $iequatableTests)
+        {
+            $iequatableTests = new IEquatableTests($this);
+        }
+        return $iequatableTests;
     }
 
 
