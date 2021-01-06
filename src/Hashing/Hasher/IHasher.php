44763b9ef05aa10b523794d23898b24b3c9248bc
Evan Washkow <epwashkow@gmail.com>
2021-01-05 19:37:56 -0800

    Define IHasher interface

---
+++ src/Hashing/Hasher/IHasher.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+namespace PHP\Hashing\Hasher;
+
+use PHP\Collections\ByteArray;
+
+/**
+ * Describes a class that computes the hash sum for any value, regardless of type.
+ */
+interface IHasher
+{
+
+
+    /**
+     * Compute the hash sum for a value, regardless of its type.
+     *
+     * @param $value The value to compute the hash sum of
+     * @return ByteArray
+     */
+    public function hash($value): ByteArray;
+}
