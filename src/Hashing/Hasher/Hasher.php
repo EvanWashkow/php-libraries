1adf38244460e34802d97b1ed2acb817e30e7b78
Evan Washkow <epwashkow@gmail.com>
2021-01-05 20:36:34 -0800

    Implement Hasher->hash()

--- src/Hashing/Hasher/Hasher.php
+++ src/Hashing/Hasher/Hasher.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 namespace PHP\Hashing\Hasher;
 
 use PHP\Collections\ByteArray;
+use PHP\HashAlgorithm\SHA256;
+use PHP\Serialization\PHPSerialization;
 
 /**
  * Computes the hash sum of a value, regardless of type
@@ -15,6 +17,28 @@ class Hasher implements IHasher
     public function hash($value): ByteArray
     {
         $hash = null;
+        if (true === $value)
+        {
+            $hash = new ByteArray(1, 1);
+        }
+        elseif (false === $value)
+        {
+            $hash = new ByteArray(0, 1);
+        }
+        elseif (is_float($value) || is_int($value) || is_string($value))
+        {
+            $hash = new ByteArray($value);
+        }
+        elseif (is_object($value))
+        {
+            $hash = new ByteArray(spl_object_hash($value));
+        }
+        else
+        {
+            $hash = (new SHA256())->hash(
+                (new PHPSerialization())->serialize($value)
+            );
+        }
         return $hash;
     }
 }
--- tests/Hashing/Hasher/HasherTest.php
+++ tests/Hashing/Hasher/HasherTest.php
@@ -3,8 +3,11 @@ declare(strict_types=1);
 
 namespace PHP\Tests\Hashing\Hasher;
 
+use PHP\Collections\ByteArray;
+use PHP\HashAlgorithm\SHA256;
 use PHP\Hashing\Hasher\Hasher;
 use PHP\Hashing\Hasher\IHasher;
+use PHP\Serialization\PHPSerialization;
 
 /**
  * Tests for the Hasher class
@@ -22,5 +25,69 @@ class HasherTest extends \PHPUnit\Framework\TestCase
             IHasher::class,
             new Hasher()
         );
+    }
+
+
+    /**
+     * Test hash() result
+     *
+     * @dataProvider getHashTestData
+     *
+     * @param $value
+     * @param ByteArray $hash
+     * @param bool $isEqual
+     */
+    public function testHash($value, ByteArray $hash, bool $isEqual): void
+    {
+        $hashString      = $hash->__toString();
+        $valueHashString = (new Hasher())->hash($value)->__toString();
+        if ($isEqual)
+        {
+            $this->assertEquals(
+                $hashString,
+                $valueHashString,
+                'Hashing the value should return the ByteArray, but did not.'
+            );
+        }
+        else
+        {
+            $this->assertNotEquals(
+                $hashString,
+                $valueHashString,
+                'Hashing the value should not return the ByteArray, but did.'
+            );
+        }
+    }
+
+    public function getHashTestData(): array
+    {
+        // Object values
+        $stdClass1 = new \stdClass();
+        $stdClass2 = new \stdClass();
+
+        // Array values
+        $array1 = [ 1, 2, 3 ];
+        $array2 = [ 'a', 'b', 'c' ];
+
+        // Hash Algorithm and Serializer
+        $hashAlgorithm = new SHA256();
+        $serializer    = new PHPSerialization();
+
+        return [
+            'true'                    => [ true,       new ByteArray(1, 1),                         true ],
+            'false'                   => [ false,      new ByteArray(0, 1),                         true ],
+            '1'                       => [ 1,          new ByteArray(1),                            true ],
+            '2'                       => [ 2,          new ByteArray(2),                            true ],
+            '"abc"'                   => [ 'abc',      new ByteArray('abc'),                        true ],
+            '"xyz"'                   => [ 'xyz',      new ByteArray('xyz'),                        true ],
+            '1.5'                     => [ 1.5,        new ByteArray(1.5),                          true ],
+            '2.0'                     => [ 2.0,        new ByteArray(2.0),                          true ],
+            'stdClass1'               => [ $stdClass1, new ByteArray(spl_object_hash($stdClass1)),          true ],
+            'stdClass2'               => [ $stdClass2, new ByteArray(spl_object_hash($stdClass2)),          true ],
+            'stdClass1 !== stdClass2' => [ $stdClass1, new ByteArray(spl_object_hash($stdClass2)),          false ],
+            'array1'                  => [ $array1, $hashAlgorithm->hash($serializer->serialize($array1)),  true ],
+            'array2'                  => [ $array2, $hashAlgorithm->hash($serializer->serialize($array2)),  true ],
+            'array1 !== array2'       => [ $array1, $hashAlgorithm->hash($serializer->serialize($array2)),  false ]
+        ];
     }
 }
