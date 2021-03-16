e77ac154eaff41f42ab9bb29c3edcade5e6ef8cc
Evan Washkow <epwashkow@gmail.com>
2021-03-16 09:38:00 -0700

    Stub TypeImplementationTestDefinition

---
+++ tests-php/Type/Model/TestDefinition/TypeImplementationTestDefinition.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace PHP\Tests\Type\Model\TestDefinition;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Defines a test for Type implementations
+ */
+abstract class TypeImplementationTestDefinition extends TestCase
+{
+
+}
