7d9f0f7c9d48941d92aabda793a5cf82fefffde3
Evan Washkow <epwashkow@gmail.com>
2021-03-16 20:47:11 -0700

    Implement ArrayType

---
+++ src-php/Type/Model/ArrayType.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace PHP\Type\Model;
+
+/**
+ * Defines an array type
+ */
+class ArrayType extends Type
+{
+    public function __construct()
+    {
+        parent::__construct('array');
+    }
+
+
+    final public function isValueOfType($value): bool
+    {
+        return is_array($value);
+    }
+
+
+    final protected function isOfType(Type $type): bool
+    {
+        return true;
+    }
+
+
+    final protected function isOfTypeName(string $typeName): bool
+    {
+        return $typeName === $this->getName();
+    }
+}
---
+++ tests-php/Type/Model/ArrayTypeTest.php
@@ -0,0 +1,53 @@
+<?php
+declare(strict_types=1);
+
+namespace PHP\Tests\Type\Model;
+
+use PHP\Type\Model\ArrayType;
+use PHP\Type\Model\Type;
+
+/**
+ * Tests the ArrayType class
+ */
+final class ArrayTypeTest extends TestDefinition\TypeImplementationTestDefinition
+{
+    public function getIsTestData(): array
+    {
+        $mockArrayType = $this->createMock(ArrayType::class);
+
+        return [
+
+            // Same type
+            'is(array)'         => ['array',         true],
+            'is(ArrayType)'     => [new ArrayType(), true],
+            'is(MockArrayType)' => [$mockArrayType,  true],
+
+            /**
+             * Different types
+             *
+             * @todo Add different Type instances to this test
+             */
+            'is(bool)'    => ['bool',    false],
+            'is(float)'   => ['float',   false],
+            'is(integer)' => ['integer', false],
+        ];
+    }
+
+
+    public function getIsValueOfTypeTestData(): array
+    {
+        return [
+            'isValueOfType([])'      => [[],      true],
+            'isValueOfType([1,2,3])' => [[1,2,3], true],
+            'isValueOfType(1)'       => [1,       false],
+            'isValueOfType(2.7)'     => [2.7,     false],
+            'isValueOfType(false)'   => [false,   false],
+        ];
+    }
+
+
+    protected function createType(): Type
+    {
+        return new ArrayType();
+    }
+}
