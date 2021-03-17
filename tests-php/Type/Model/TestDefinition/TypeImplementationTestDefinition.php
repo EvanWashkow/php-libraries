a42984491bf8956ffc61087f9ddd84fe05e6e7ce
Evan Washkow <epwashkow@gmail.com>
2021-03-16 19:27:43 -0700

    Write methods around the getting the Type to test

--- tests-php/Type/Model/TestDefinition/TypeImplementationTestDefinition.php
+++ tests-php/Type/Model/TestDefinition/TypeImplementationTestDefinition.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace PHP\Tests\Type\Model\TestDefinition;
 
+use PHP\Type\Model\Type;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -10,5 +11,25 @@ use PHPUnit\Framework\TestCase;
  */
 abstract class TypeImplementationTestDefinition extends TestCase
 {
+    /** @var ?Type The Type instance */
+    private $type = null;
 
+
+    /**
+     * Creates a new Type instance to be tested
+     */
+    abstract protected function createType(): Type;
+
+
+    /**
+     * Creates or returns a singleton instance of the Type to be tested
+     */
+    final protected function getOrCreateType(): Type
+    {
+        if (! $this->type instanceof Type)
+        {
+            $this->type = $this->createType();
+        }
+        return $this->type;
+    }
 }
