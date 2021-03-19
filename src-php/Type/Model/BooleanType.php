fcba2b9f3136884fcb28e8cff373d7ecca66985b
Evan Washkow <epwashkow@gmail.com>
2021-03-19 16:02:19 -0700

    Implement BooleanType

---
+++ src-php/Type/Model/BooleanType.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types = 1);
+
+namespace PHP\Type\Model;
+
+/**
+ * Defines a boolean type
+ */
+class BooleanType extends Type
+{
+    public function __construct()
+    {
+        parent::__construct('boolean');
+    }
+
+
+    final public function isValueOfType($value): bool
+    {
+        return is_bool($value);
+    }
+
+
+    final protected function isOfType(Type $type): bool
+    {
+        return $type instanceof BooleanType;
+    }
+
+
+    final protected function isOfTypeName(string $typeName): bool
+    {
+        return in_array($typeName, [$this->getName(), 'bool'], true);
+    }
+}
---
+++ tests-php/Type/Model/BooleanTypeTest.php
@@ -0,0 +1,65 @@
+<?php
+declare(strict_types = 1);
+
+namespace PHP\Tests\Type\Model;
+
+use PHP\Type\Model\ArrayType;
+use PHP\Type\Model\BooleanType;
+use PHP\Type\Model\FloatType;
+use PHP\Type\Model\Type;
+
+/**
+ * Tests the BooleanType class
+ */
+final class BooleanTypeTest extends TestDefinition\StaticTypeTestDefinition
+{
+    public function getIsTestData(): array
+    {
+        return [
+
+            // Same type
+            'is(bool)' => ['bool', true],
+            'is(boolean)' => ['boolean', true],
+            'is(BooleanType)' => [$this->createType(), true],
+            'is(MockBooleanType)' => [
+                $this->createMock(BooleanType::class),
+                true
+            ],
+
+            /**
+             * Different types
+             *
+             * @todo Add different Type instances to this test
+             */
+            'is(array)' => ['array', false],
+            'is(ArrayType)' => [new ArrayType(), false],
+            'is(float)' => ['float', false],
+            'is(FloatType)' => [new FloatType(), false],
+            'is(integer)' => ['integer', false],
+        ];
+    }
+
+
+    public function getIsValueOfTypeTestData(): array
+    {
+        return [
+            'isValueOfType(true)' => [true, true],
+            'isValueOfType(false)' => [false, true],
+            'isValueOfType([])' => [[], false],
+            'isValueOfType(1)' => [1, false],
+            'isValueOfType(2.7)' => [2.7, false],
+        ];
+    }
+
+
+    protected function createType(): Type
+    {
+        return new BooleanType();
+    }
+
+
+    protected function getExpectedTypeName(): string
+    {
+        return 'boolean';
+    }
+}
