9d8257c667ccbadc0cc4005b30670b08573b14e5
Evan Washkow <epwashkow@gmail.com>
2021-03-18 23:02:13 -0700

    Implement FloatType

---
+++ src-php/Type/Model/FloatType.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace PHP\Type\Model;
+
+/**
+ * Defines a floating point number type
+ */
+class FloatType extends Type
+{
+    public function __construct()
+    {
+        parent::__construct('float');
+    }
+
+
+    public function isValueOfType($value): bool
+    {
+        return is_float($value);
+    }
+
+
+    protected function isOfType(Type $type): bool
+    {
+        return $type instanceof FloatType;
+    }
+
+
+    protected function isOfTypeName(string $typeName): bool
+    {
+        return in_array($typeName, [$this->getName(), 'double'], true);
+    }
+}
---
+++ tests-php/Type/Model/FloatTypeTest.php
@@ -0,0 +1,64 @@
+<?php
+declare(strict_types=1);
+
+namespace PHP\Tests\Type\Model;
+
+use PHP\Type\Model\ArrayType;
+use PHP\Type\Model\FloatType;
+use PHP\Type\Model\Type;
+
+/**
+ * Tests the FloatType class
+ */
+final class FloatTypeTest extends TestDefinition\StaticTypeTestDefinition
+{
+    public function getIsTestData(): array
+    {
+        return [
+
+            // Same type
+            'is(double)' => ['double', true],
+            'is(float)' => ['float', true],
+            'is(FloatType)' => [$this->createType(), true],
+            'is(MockFloatType)' => [
+                $this->createMock(FloatType::class),
+                true
+            ],
+
+            /**
+             * Different types
+             *
+             * @todo Add different Type instances to this test
+             */
+            'is(array)' => ['array', false],
+            'is(ArrayType)' => [new ArrayType(), false],
+            'is(bool)' => ['bool', false],
+            'is(integer)' => ['integer', false],
+        ];
+    }
+
+
+    public function getIsValueOfTypeTestData(): array
+    {
+        return [
+            'isValueOfType(-8.9)' => [-8.9, true],
+            'isValueOfType(1.0)' => [1.0, true],
+            'isValueOfType(31.4)' => [31.4, true],
+            'isValueOfType([])' => [[], false],
+            'isValueOfType(false)' => [false, false],
+            'isValueOfType(1)' => [1, false],
+        ];
+    }
+
+
+    protected function createType(): Type
+    {
+        return new FloatType();
+    }
+
+
+    protected function getExpectedTypeName(): string
+    {
+        return 'float';
+    }
+}
