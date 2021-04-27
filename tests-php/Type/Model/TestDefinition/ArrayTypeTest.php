05d1b1fecde1a4f3431fdf0ddd24733078f0dd88
Evan Washkow <epwashkow@gmail.com>
2021-04-27 10:00:45 -0700

    test: refactor old ArrayTypeTest

---
+++ tests-php/Type/Model/TestDefinition/ArrayTypeTest.php
@@ -0,0 +1,55 @@
+<?php
+declare(strict_types=1);
+
+namespace PHP\Tests\Type\Model\TestDefinition;
+
+use PHP\Type\Model\ArrayType;
+use PHP\Type\Model\BooleanType;
+use PHP\Type\Model\FloatType;
+use PHP\Type\Model\IntegerType;
+
+final class ArrayTypeTest extends TypeTestDefinition
+{
+    public function getIsTestData(): array
+    {
+        $type = new ArrayType();
+        $childType = new class extends ArrayType {};
+        return [
+            'ArrayType' => [$type, $type, true],
+            'ChildType->is(ArrayType)' => [$childType, $type, true],
+            'ArrayType->is(ChildType)' => [$type, $childType, true],
+            'BooleanType' => [$type, new BooleanType(), false],
+            'FloatType' => [$type, new FloatType(), false],
+            'IntegerType' => [$type, new IntegerType(), false],
+        ];
+    }
+
+
+    public function getIsUnknownTypeNameTestData(): array
+    {
+        return [
+            'ArrayType' => [new ArrayType()]
+        ];
+    }
+
+
+    public function getIsValueOfTypeTestData(): array
+    {
+        $type = new ArrayType();
+        return [
+            '[]' => [$type, [], true],
+            '[1,2,3]' => [$type, [1,2,3], true],
+            '1' => [$type, 1, false],
+            '2.7' => [$type, 2.7, false],
+            'false' => [$type, false, false],
+        ];
+    }
+
+
+    public function getNamesTestData(): array
+    {
+        return [
+            'ArrayType' => [new ArrayType(), 'array'],
+        ];
+    }
+}
