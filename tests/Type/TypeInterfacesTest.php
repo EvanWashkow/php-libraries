6f351ca650d1f10b4a7906f9faf315efb45ae724
Evan Washkow <epwashkow@gmail.com>
2022-01-15 13:12:51 -0800

    Define NameableTypeInterface

---
+++ tests/Type/TypeInterfacesTest.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace EvanWashkow\PHPLibraries\Tests\Type;
+
+use EvanWashkow\PHPLibraries\Type\NameableTypeInterface;
+use EvanWashkow\PHPLibraries\Type\TypeInterface;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Tests Type interfaces.
+ */
+final class TypeInterfacesTest extends TestCase
+{
+    /**
+     * @dataProvider getTestData
+     */
+    public function test($interface, $expectedParent): void
+    {
+        $interfaceReflection = new \ReflectionClass($interface);
+        $expectedReflection = new \ReflectionClass($expectedParent);
+        $this->assertTrue(
+            $interfaceReflection->isSubclassOf($expectedReflection),
+            "{$interface} does not derive from {$expectedParent}"
+        );
+    }
+
+    public function getTestData(): array
+    {
+        return [
+            NameableTypeInterface::class => [NameableTypeInterface::class, TypeInterface::class],
+        ];
+    }
+}
