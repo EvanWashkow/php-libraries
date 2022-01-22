618ec285bed801c2fadf95c3564d9f218c5db521
Evan Washkow <epwashkow@gmail.com>
2022-01-21 22:25:18 -0800

    Remove duplicate tests from OldTypeTest

--- tests/Type/OldTypeTest.php
+++ tests/Type/OldTypeTest.php
@@ -19,81 +19,6 @@ use PHPUnit\Framework\TestCase;
 final class OldTypeTest extends TestCase
 {
     /**
-     * @dataProvider getFinalTestData
-     */
-    public function testFinal(TypeInterface $type): void
-    {
-        $rc = new \ReflectionClass($type);
-        $this->assertTrue($rc->isFinal(), "Type is not final");
-    }
-
-    public function getFinalTestData(): array
-    {
-        $data = [];
-        foreach ($this->getTestBuilders() as $builder) {
-            $type = $builder->getType();
-            $data[get_class($type)] = [$type];
-        }
-        return $data;
-    }
-
-
-    /**
-     * @dataProvider getConstructorExceptionTestData
-     */
-    public function testConstructorException(callable $fn, string $expected): void
-    {
-        $this->expectException($expected);
-        $fn();
-    }
-
-    public function getConstructorExceptionTestData(): array
-    {
-        $classType = ClassType::class;
-        $interfaceType = InterfaceType::class;
-
-        return [
-            "{$classType}('')" => [
-                function() {
-                    new ClassType('');
-                },
-                \DomainException::class
-            ],
-            "{$classType}('foobar')" => [
-                function() {
-                    new ClassType('foobar');
-                },
-                \DomainException::class
-            ],
-            "{$classType}(StubInterfaceA)" => [
-                function() {
-                    new ClassType(StubInterfaceA::class);
-                },
-                \DomainException::class
-            ],
-            "{$interfaceType}('')" => [
-                function() {
-                    new InterfaceType('');
-                },
-                \DomainException::class
-            ],
-            "{$interfaceType}('foobar')" => [
-                function() {
-                    new InterfaceType('foobar');
-                },
-                \DomainException::class
-            ],
-            "{$interfaceType}(StubClassA)" => [
-                function() {
-                    new InterfaceType(StubClassA::class);
-                },
-                \DomainException::class
-            ],
-        ];
-    }
-
-
-    /**
      * @dataProvider getIsTestData
      */
     public function testIs(TypeInterface $tester, TypeInterface $testee, bool $expected): void
@@ -112,24 +37,6 @@ final class OldTypeTest extends TestCase
 
 
     /**
-     * @dataProvider getIsValueOfTypeTestData
-     */
-    public function testIsValueOfType(TypeInterface $type, $value, bool $expected): void
-    {
-        $this->assertSame($expected, $type->isValueOfType($value));
-    }
-
-    public function getIsValueOfTypeTestData(): array
-    {
-        $data = [];
-        foreach ($this->getTestBuilders() as $builder) {
-            $data = array_merge($data, $builder->buildIsValueOfTypeTestData());
-        }
-        return $data;
-    }
-
-
-    /**
      * Retrieves the TypeTestDataBuilders
      *
      * @return array<TypeTestDataBuilder>
@@ -140,106 +47,31 @@ final class OldTypeTest extends TestCase
         $interfaceType = InterfaceType::class;
 
         return [
-            $this->newTestBuilder(ArrayType::class, new ArrayType())
-                ->isValueOfType('empty array', [])
-                ->isValueOfType('full array', [1, 2, 3])
-                ->notIsValueOfType('bool', false)
-                ->notIsValueOfType('float', 3.1415)
-                ->notIsValueOfType('integer', 1)
-                ->notIsValueOfType('string', 'string'),
-            $this->newTestBuilder(BooleanType::class, new BooleanType())
-                ->isValueOfType('true', true)
-                ->isValueOfType('false', false)
-                ->notIsValueOfType('array', [])
-                ->notIsValueOfType('float', 3.1415)
-                ->notIsValueOfType('integer', 1)
-                ->notIsValueOfType('string', 'string'),
-            $this->newTestBuilder(FloatType::class, new FloatType())
-                ->isValueOfType('3.1415', 3.1415)
-                ->isValueOfType('1.5', 1.5)
-                ->isValueOfType('-2.9', -2.9)
-                ->notIsValueOfType('array', [])
-                ->notIsValueOfType('bool', false)
-                ->notIsValueOfType('integer', 1)
-                ->notIsValueOfType('string', 'string'),
-            $this->newTestBuilder(IntegerType::class, new IntegerType())
-                ->isValueOfType('PHP_INT_MAX', PHP_INT_MAX)
-                ->isValueOfType('1', 1)
-                ->isValueOfType('-1', -1)
-                ->isValueOfType('PHP_INT_MIN', PHP_INT_MIN)
-                ->notIsValueOfType('array', [])
-                ->notIsValueOfType('bool', false)
-                ->notIsValueOfType('float', 3.1415)
-                ->notIsValueOfType('string', 'string'),
-            $this->newTestBuilder(StringType::class, new StringType())
-                ->isValueOfType('string', 'string')
-                ->isValueOfType('empty string', '')
-                ->notIsValueOfType('array', [])
-                ->notIsValueOfType('bool', false)
-                ->notIsValueOfType('float', 3.1415)
-                ->notIsValueOfType('integer', 1),
-
             $this->newTestBuilder("{$classType}(StubClassA)", new ClassType(StubClassA::class))
                 ->is('StubInterfaceA', new InterfaceType(StubInterfaceA::class))
                 ->notIs('StubInterfaceB', new InterfaceType(StubInterfaceB::class))
                 ->notIs('StubInterfaceC', new InterfaceType(StubInterfaceC::class))
                 ->notIs('StubClassB', new ClassType(StubClassB::class))
-                ->notIs('StubClassC', new ClassType(StubClassC::class))
-                ->isValueOfType('mockStubClassA', $this->createMock(StubClassA::class))
-                ->isValueOfType('StubClassA', new StubClassA())
-                ->isValueOfType('StubClassB', new StubClassB())
-                ->isValueOfType('StubClassC', new StubClassC())
-                ->notIsValueOfType('array', [])
-                ->notIsValueOfType('bool', false)
-                ->notIsValueOfType('float', 3.1415)
-                ->notIsValueOfType('integer', 1)
-                ->notIsValueOfType('string', 'string'),
+                ->notIs('StubClassC', new ClassType(StubClassC::class)),
             $this->newTestBuilder("{$classType}(StubClassB)", new ClassType(StubClassB::class))
                 ->is('StubClassA', new ClassType(StubClassA::class))
                 ->is('StubInterfaceA', new InterfaceType(StubInterfaceA::class))
                 ->is('StubInterfaceB', new InterfaceType(StubInterfaceB::class))
                 ->notIs('StubInterfaceC', new InterfaceType(StubInterfaceC::class))
-                ->notIs('StubClassC', new ClassType(StubClassC::class))
-                ->isValueOfType('mockStubClassB', $this->createMock(StubClassB::class))
-                ->isValueOfType('StubClassB', new StubClassB())
-                ->notIsValueOfType('StubClassA', new StubClassA())
-                ->notIsValueOfType('StubClassC', new StubClassC())
-                ->notIsValueOfType('array', [])
-                ->notIsValueOfType('bool', false)
-                ->notIsValueOfType('float', 3.1415)
-                ->notIsValueOfType('integer', 1)
-                ->notIsValueOfType('string', 'string'),
+                ->notIs('StubClassC', new ClassType(StubClassC::class)),
 
             $this->newTestBuilder("{$interfaceType}(StubInterfaceA)", new InterfaceType(StubInterfaceA::class))
                 ->notIs('StubInterfaceB', new InterfaceType(StubInterfaceB::class))
                 ->notIs('StubInterfaceC', new InterfaceType(StubInterfaceC::class))
                 ->notIs('StubClassA', new ClassType(StubClassA::class))
                 ->notIs('StubClassB', new ClassType(StubClassB::class))
-                ->notIs('StubClassC', new ClassType(StubClassC::class))
-                ->isValueOfType('mockStubInterfaceA', $this->createMock(StubInterfaceA::class))
-                ->isValueOfType('StubClassA', new StubClassA())
-                ->isValueOfType('StubClassB', new StubClassB())
-                ->isValueOfType('StubClassC', new StubClassC())
-                ->notIsValueOfType('array', [])
-                ->notIsValueOfType('bool', false)
-                ->notIsValueOfType('float', 3.1415)
-                ->notIsValueOfType('integer', 1)
-                ->notIsValueOfType('string', 'string'),
+                ->notIs('StubClassC', new ClassType(StubClassC::class)),
             $this->newTestBuilder("{$interfaceType}(StubInterfaceB)", new InterfaceType(StubInterfaceB::class))
                 ->is('StubInterfaceA', new InterfaceType(StubInterfaceA::class))
                 ->notIs('StubInterfaceC', new InterfaceType(StubInterfaceC::class))
                 ->notIs('StubClassA', new ClassType(StubClassA::class))
                 ->notIs('StubClassB', new ClassType(StubClassB::class))
-                ->notIs('StubClassC', new ClassType(StubClassC::class))
-                ->isValueOfType('StubClassB', new StubClassB())
-                ->isValueOfType('mockStubInterfaceB', $this->createMock(StubInterfaceB::class))
-                ->notIsValueOfType('StubClassA', new StubClassA())
-                ->notIsValueOfType('StubClassC', new StubClassC())
-                ->notIsValueOfType('array', [])
-                ->notIsValueOfType('bool', false)
-                ->notIsValueOfType('float', 3.1415)
-                ->notIsValueOfType('integer', 1)
-                ->notIsValueOfType('string', 'string'),
+                ->notIs('StubClassC', new ClassType(StubClassC::class)),
         ];
     }
 
