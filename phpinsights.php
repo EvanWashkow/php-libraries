b4a60fc0c559e5cf801a08fd0ed695b15c9f9d56
Evan Washkow <epwashkow@gmail.com>
2022-07-02 23:09:17 -0700

    ci: configure phpinsights to ignore old src and tests directories

---
+++ phpinsights.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'preset' => 'default',
+    'exclude' => [
+        'src-php',
+        'tests-php',
+    ],
+    'add' => [
+        //  ExampleMetric::class => [
+        //      ExampleInsight::class,
+        //  ]
+    ],
+    'remove' => [
+        //  ExampleInsight::class,
+    ],
+    'config' => [
+        //  ExampleInsight::class => [
+        //      'key' => 'value',
+        //  ],
+    ],
+];
