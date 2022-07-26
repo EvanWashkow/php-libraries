4b97a9782ac01cc2a4ff01e0a5965cb7feada077
Evan Washkow <epwashkow@gmail.com>
2022-07-25 22:39:10 -0700

    refactor: use PrimitiveKeyHashMapHelper in IntegerKeyHashMap

--- src/Collection/IntegerKeyHashMap.php
+++ src/Collection/IntegerKeyHashMap.php
@@ -6,7 +6,6 @@ namespace EvanWashkow\PHPLibraries\Collection;
 
 use EvanWashkow\PHPLibraries\CollectionInterface\Mapper;
 use EvanWashkow\PHPLibraries\Type\IntegerType;
-use EvanWashkow\PHPLibraries\Type\StringType;
 use EvanWashkow\PHPLibraries\TypeInterface\Type;
 
 /**
@@ -14,10 +13,7 @@ use EvanWashkow\PHPLibraries\TypeInterface\Type;
  */
 final class IntegerKeyHashMap implements Mapper
 {
-    /** @var array<int|string, mixed> The hash map */
-    private array $hashMap;
-    private Type $keyType;
-    private Type $valueType;
+    private PrimitiveKeyHashMapHelper $helper;
 
     /**
      * Create a new IntegerKeyHashMap instance
@@ -25,96 +21,48 @@ final class IntegerKeyHashMap implements Mapper
      * @param Type $valueType The value type requirement for all values in the map
      */
     public function __construct(Type $valueType) {
-        $this->hashMap = [];
-        $this->keyType = new IntegerType();
-        $this->valueType = $valueType;
+        $this->helper = new PrimitiveKeyHashMapHelper(new IntegerType(), $valueType);
     }
 
     public function count(): int {
-        return count($this->hashMap);
+        return $this->helper->count();
     }
 
     /**
      * @inheritDoc
      */
     public function get($key) {
-        $this->throwOnInvalidKeyType($key);
-        $this->throwOnMissingKey($key);
-        return $this->hashMap[$key];
+        return $this->helper->get($key);
     }
 
     public function getKeyType(): Type {
-        return $this->keyType;
+        return $this->helper->getKeyType();
     }
 
     public function getValueType(): Type {
-        return $this->valueType;
+        return $this->helper->getValueType();
     }
 
     /**
      * @inheritDoc
      */
     public function hasKey($key): bool {
-        $this->throwOnInvalidKeyType($key);
-        return array_key_exists($key, $this->hashMap);
+        return $this->helper->hasKey($key);
     }
 
     /**
      * @inheritDoc
      */
-    public function removeKey($key): HashMap {
-        $this->throwOnInvalidKeyType($key);
-        $this->throwOnMissingKey($key);
-        unset($this->hashMap[$key]);
+    public function removeKey($key): IntegerKeyHashMap {
+        $this->helper->removeKey($key);
         return $this;
     }
 
     /**
      * @inheritDoc
      */
-    public function set($key, $value): HashMap {
-        $this->throwOnInvalidKeyType($key);
-        $this->throwOnInvalidValueType($value);
-        $this->hashMap[$key] = $value;
+    public function set($key, $value): IntegerKeyHashMap {
+        $this->helper->set($key, $value);
         return $this;
-    }
-
-    /**
-     * Throws an exception on an invalid key type
-     *
-     * @param int|string $key The key
-     *
-     * @throws \InvalidArgumentException
-     */
-    private function throwOnInvalidKeyType($key): void {
-        if (! $this->getKeyType()->isValueOfType($key)) {
-            throw new \InvalidArgumentException('The key is the wrong type');
-        }
-    }
-
-    /**
-     * Throws an exception on an invalid value type
-     *
-     * @param mixed $value The value
-     *
-     * @throws \InvalidArgumentException
-     */
-    private function throwOnInvalidValueType($value): void {
-        if (! $this->getValueType()->isValueOfType($value)) {
-            throw new \InvalidArgumentException('Cannot set value: the value is the wrong type');
-        }
-    }
-
-    /**
-     * Throws an exception on a missing key
-     *
-     * @param int|string $key
-     *
-     * @throws \OutOfBoundsException
-     */
-    private function throwOnMissingKey($key): void {
-        if (! $this->hasKey($key)) {
-            throw new \OutOfBoundsException('The key does not exist');
-        }
     }
 }
