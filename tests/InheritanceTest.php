ff0773e9f2eb166027c7b463e8ceb230cc839539
Evan Washkow <epwashkow@gmail.com>
2022-07-26 21:56:04 -0700

    test: collection type inheritance

---
+++ tests/InheritanceTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace EvanWashkow\PHPLibraries\Tests;
+
+use EvanWashkow\PHPLibraries\Collection\HashMap;
+use EvanWashkow\PHPLibraries\Collection\IntegerKeyHashMap;
+use EvanWashkow\PHPLibraries\Collection\StringKeyHashMap;
+use EvanWashkow\PHPLibraries\CollectionInterface\Mapper;
+use EvanWashkow\PHPLibraries\Type\ClassType;
+use EvanWashkow\PHPLibraries\Type\InterfaceType;
+use EvanWashkow\PHPLibraries\TypeInterface\Type;
+
+/**
+ * Tests type inheritance
+ */
+final class InheritanceTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @param ClassType|InterfaceType $type The type
+     * @param ClassType|InterfaceType $expectedParent The type's expected parent
+     *
+     * @dataProvider getTestData
+     */
+    public function test(Type $type, Type $expectedParent) {
+        $this->assertTrue(
+            $type->is($expectedParent),
+            "{$type->getName()} does not inherit {$expectedParent->getName()}"
+        );
+    }
+
+    public function getTestData(): array {
+        return array_merge(
+            // Collection
+            $this->buildTest(new ClassType(HashMap::class), new InterfaceType(Mapper::class)),
+            $this->buildTest(new ClassType(IntegerKeyHashMap::class), new InterfaceType(Mapper::class)),
+            $this->buildTest(new ClassType(StringKeyHashMap::class), new InterfaceType(Mapper::class)),
+
+            // CollectionInterface
+            $this->buildTest(new InterfaceType(Mapper::class), new InterfaceType(\Countable::class)),
+        );
+    }
+
+    /**
+     * Build a new test entry
+     *
+     * @param ClassType|InterfaceType $type The type
+     * @param ClassType|InterfaceType ...$expectedParents The type's expected parents
+     */
+    private function buildTest(Type $type, Type ...$expectedParents): array {
+        $tests = [];
+        foreach ($expectedParents as $expectedParent) {
+            $tests["{$type->getName()} implements {$expectedParent->getName()}"] = [$type, $expectedParent];
+        }
+        return $tests;
+    }
+}
