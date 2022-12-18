db925be99eb8515b4e8c64ea0ddb768251538744
Evan Washkow <epwashkow@gmail.com>
2022-12-18 17:38:50 -0500

    chore: document assertThrows

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,6 +6,14 @@ namespace EvanWashkow\PHPLibraries\Tests;
 
 abstract class TestCase extends \PHPUnit\Framework\TestCase
 {
+    /**
+     * Checks if the closure throws an exception
+     *
+     * @param string $wantException The expected exception
+     * @param \Closure $func Function callback that throws the exception
+     * @param string|null $message Message to output on failure
+     * @return void
+     */
     public function assertThrows(string $wantException, \Closure $func, ?string $message = null): void
     {
         $gotException = null;
