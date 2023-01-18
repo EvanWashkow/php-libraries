19fd620e7044abf17926141d71797cfcfe0c0ff6
Evan Washkow <epwashkow@gmail.com>
2023-01-17 21:11:46 -0500

    feat: create Cloneable interface

---
+++ src/Cloneable.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EvanWashkow\PhpLibraries;
+
+interface Cloneable
+{
+    /**
+     * Returns a copy of the object
+     */
+    public function clone(): self;
+}
