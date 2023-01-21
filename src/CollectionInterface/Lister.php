8265b3591bb8fcda06d6b3975be06d27740a8d86
Evan Washkow <epwashkow@gmail.com>
2023-01-21 14:16:06 -0500

    docs: mark Lister as requiring integer keys

--- src/CollectionInterface/Lister.php
+++ src/CollectionInterface/Lister.php
@@ -17,4 +17,31 @@ interface Lister extends KeyedCollector
      * @return self The modified list
      */
     public function add(mixed $value): self;
+
+    /**
+     * @param int $key The key
+     *
+     * @return mixed The value
+     */
+    public function get(mixed $key): mixed;
+
+    /**
+     * @param int $key The key
+     */
+    public function hasKey(mixed $key): bool;
+
+    /**
+     * @param int $key The key to remove
+     *
+     * @return self The modified collection
+     */
+    public function removeKey(mixed $key): self;
+
+    /**
+     * @param int $key The key for the value
+     * @param mixed $value The value
+     *
+     * @return self The modified collection
+     */
+    public function set(mixed $key, mixed $value): self;
 }
