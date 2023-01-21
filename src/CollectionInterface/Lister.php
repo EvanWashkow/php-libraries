108d9cf4486c22fdbb8ec99be501cb0c11ba6fdb
Evan Washkow <epwashkow@gmail.com>
2023-01-21 15:05:54 -0500

    refactor: modify Lister to extend KeyedCollector generic template

--- src/CollectionInterface/Lister.php
+++ src/CollectionInterface/Lister.php
@@ -6,42 +6,11 @@ namespace EvanWashkow\PhpLibraries\CollectionInterface;
 
 /**
  * Describes a list of indexed values
+ *
+ * @template TValue
+ *
+ * @extends KeyedCollector<int, TValue>
  */
 interface Lister extends KeyedCollector
 {
-    /**
-     * Add a new value to the list
-     *
-     * @param mixed $value The value
-     *
-     * @return self The modified list
-     */
-    public function add(mixed $value): self;
-
-    /**
-     * @param int $key The key
-     *
-     * @return mixed The value
-     */
-    public function get(mixed $key): mixed;
-
-    /**
-     * @param int $key The key
-     */
-    public function hasKey(mixed $key): bool;
-
-    /**
-     * @param int $key The key to remove
-     *
-     * @return self The modified collection
-     */
-    public function removeKey(mixed $key): self;
-
-    /**
-     * @param int $key The key for the value
-     * @param mixed $value The value
-     *
-     * @return self The modified collection
-     */
-    public function set(mixed $key, mixed $value): self;
 }
