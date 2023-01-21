439f3939186eb62697e03f5f03c75603d4c42750
Evan Washkow <epwashkow@gmail.com>
2023-01-21 14:39:46 -0500

    docs: add generics documentation to Mapper

--- src/CollectionInterface/Mapper.php
+++ src/CollectionInterface/Mapper.php
@@ -6,33 +6,9 @@ namespace EvanWashkow\PhpLibraries\CollectionInterface;
 
 /**
  * Describes a collection with key => value mapping
+ * 
+ * @template KeyType
+ * @template ValueType
+ * @extends KeyedCollector<KeyType, ValueType>
  */
-interface Mapper extends KeyedCollector
-{
-    /**
-     * @param int|string $key The key
-     *
-     * @return mixed The value
-     */
-    public function get(mixed $key): mixed;
-
-    /**
-     * @param int|string $key The key
-     */
-    public function hasKey(mixed $key): bool;
-
-    /**
-     * @param int|string $key The key to remove
-     *
-     * @return self The modified collection
-     */
-    public function removeKey(mixed $key): self;
-
-    /**
-     * @param int|string $key The key for the value
-     * @param mixed $value The value
-     *
-     * @return self The modified collection
-     */
-    public function set(mixed $key, mixed $value): self;
-}
+interface Mapper extends KeyedCollector { }
